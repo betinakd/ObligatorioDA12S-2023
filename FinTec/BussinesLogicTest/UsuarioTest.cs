4e85131980dbbe45aaca4b424ecd34a40cf62a00
maxxi98 <maxi_nos_10@hotmail.com>
2023-09-23 20:30:38 -0300

    RED: Nuevo_Usuario

---
+++ FinTec/BussinesLogicTest/UsuarioTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLogicTest
+{
+    [TestClass]
+    internal class UsuarioTest
+    {
+        [TestMethod]
+        public void Nuevo_Usuario()
+        {
+            var usuario = new Usuario();
+            Assert.IsNotNull(usuario);
+        }
+    }
+}
