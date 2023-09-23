d2c326a509601fbcb7877cdf2b5218dbfefc8d3b
maxxi98 <maxi_nos_10@hotmail.com>
2023-09-23 20:42:56 -0300

    GREEN: Nuevo_Usuario

--- FinTec/BussinesLogicTest/UsuarioTest.cs
+++ FinTec/BussinesLogicTest/UsuarioTest.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BussinesLogic;
 
 namespace BussinesLogicTest
 {
     [TestClass]
-    internal class UsuarioTest
+    public class UsuarioTest
     {
         [TestMethod]
         public void Nuevo_Usuario()
