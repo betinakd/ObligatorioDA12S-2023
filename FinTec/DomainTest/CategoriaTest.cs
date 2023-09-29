cfa387b8b68dea12b47acf471dce23818195784a
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-09-29 14:48:59 -0300

    RED: Nueva_Categoria

---
+++ FinTec/DomainTest/CategoriaTest.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+
+namespace DomainTest
+{
+	[TestClass]
+	public class CategoriaTest
+	{
+		[TestMethod]
+		public void Nueva_Categoria()
+		{
+			var categoria = new Categoria();
+			Assert.IsNotNull(categoria);
+		}
+	}
+}
