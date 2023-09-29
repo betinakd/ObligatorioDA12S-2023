6e89e74dc0386d4e779b6b160d18e9c8617cd7de
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-09-29 20:33:50 -0300

    RED: Nueva_Cuenta_No_Nula

---
+++ FinTec/DomainTest/CuentaTest.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+
+namespace DomainTest
+{
+	[TestClass]
+	public class CuentaTest
+	{
+		private Cuenta cuenta;
+
+		[TestInitialize]
+		public void Inicializar()
+		{
+			cuenta = new Cuenta();
+		}
+
+		[TestMethod]
+		public void Nueva_Cuenta()
+		{
+			Assert.IsNotNull(cuenta);
+		}
+	}
+}
