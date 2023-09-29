83082d80df59c8167c72c6ebd057755ef28c4048
maxxi98 <maxi_nos_10@hotmail.com>
2023-09-29 15:03:11 -0300

    Creacion DomainExceptions

---
+++ FinTec/Domain/DomainExceptions/RequiredPropertyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DomainExceptions
+{
+    internal class RequiredPropertyException : Exception
+    {
+        public RequiredPropertyException(string message) : base(message)
+        {
+        }
+    }
+}
