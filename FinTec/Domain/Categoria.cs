096495ff3bedfb19d8ad0712bf21f1877a0e99b2
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-09-29 17:16:39 -0300

    GREEN: Categoria_Tiene_FechaCreacion

--- FinTec/Domain/Categoria.cs
+++ FinTec/Domain/Categoria.cs
@@ -5,6 +5,7 @@ namespace Domain
 	public class Categoria
 	{
 		private string _nombre;
+		private readonly DateTime _fechaCreacion = DateTime.Now;
 		public string Nombre
 		{
 			get { return _nombre; }
@@ -16,6 +17,11 @@ namespace Domain
 				}
 				_nombre = value;
 			}
+		}
+
+		public DateTime FechaCreacion
+		{
+			get { return _fechaCreacion; }
 		}
 		public Categoria()
 		{
