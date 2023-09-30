ec74ee58d4d6cf22469e7be9a7a9cec82920f019
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-09-29 22:54:18 -0300

    RED: Nueva_Cuenta_Ahorro_No_Nula

---
+++ FinTec/DomainTest/AhorroTest.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+namespace DomainTest
+{
+	[TestClass]
+	public class AhorroTest
+	{
+		private Cuenta cuentaAhorro;
+
+		[TestInitialize]
+		public void Inicializar()
+		{
+			Cuenta cuentaAhorro = new Ahorro();
+		}
+
+		[TestMethod]
+		public void Nueva_Cuenta_Ahorro_No_Nula()
+		{
+			Assert.IsNotNull(cuentaAhorro);
+		}
+	}
+}
