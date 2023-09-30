7eb62c10f158b0b95559e760f3d9bcef511751c0
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-09-30 16:14:17 -0300

    RED: Creditor_Tiene_Banco_Emisor

--- FinTec/DomainTest/CreditoTest.cs
+++ FinTec/DomainTest/CreditoTest.cs
@@ -10,5 +10,13 @@ namespace DomainTest
 			Credito cuentaCredito = new Credito();
 			Assert.IsNotNull(cuentaCredito);
 		}
+
+		[TestMethod]
+		public void Credito_Tiene_Banco_Emisor()
+		{
+			Credito cuentaCredito = new Credito();
+			cuentaCredito.BancoEmisor = "BancoEmisorPrueba";
+			Assert.AreEqual("BancoEmisorPrueba", cuentaCredito.BancoEmisor);
+		}
 	}
 }
