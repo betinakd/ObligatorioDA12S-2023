425f103d9d7459fdd3093f1ce70a97a1b79bcf29
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-09-30 00:03:35 -0300

    GREEN: Excepcion_CuentaAhorro_Monto_Negativo

--- FinTec/Domain/Ahorro.cs
+++ FinTec/Domain/Ahorro.cs
@@ -3,22 +3,37 @@
 	public class Ahorro : Cuenta
 	{
 		private string _nombre;
-		public string Nombre 
+		private double _monto;
+		public string Nombre
 		{
 			get
-			{ 
+			{
 				return _nombre;
 			}
 			set
-			{ 
-				if(string.IsNullOrEmpty(value))
+			{
+				if (string.IsNullOrEmpty(value))
 				{
 					throw new DomainCuentaException("El nombre de la cuenta no puede ser vacío");
 				}
 				_nombre = value;
 			}
 		}
-		public double Monto { get; set; }
+		public double Monto
+		{
+			get
+			{
+				return _monto;
+			}
+			set
+			{
+				if (value < 0)
+				{
+					throw new DomainCuentaException("El monto de la cuenta no puede ser negativo");
+				}
+				_monto = value;
+			}
+		}
 		public Ahorro()
 		{
 		}
