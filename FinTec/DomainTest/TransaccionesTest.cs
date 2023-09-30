3f2d87360ab963f647e53605b8257a6157619eb3
maxxi98 <maxi_nos_10@hotmail.com>
2023-09-30 17:13:51 -0300

    RED: Nueva_Transaccion

---
+++ FinTec/DomainTest/TransaccionesTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DomainTest
+{
+    [TestClass]
+    public class TransaccionesTest
+    {
+        [TestMethod]
+        public void Nueva_Transaccion()
+        {
+            var transaccion = new Transaccion();
+            Assert.IsNotNull(transaccion);
+        }
+    }
+}
