9287c7f39b86489d677ffc4ecdcaff9d24d6e91d
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-09-30 16:09:41 -0300

    RED: Nueva_Cuenta_Credito_No_Nula

---
+++ FinTec/DomainTest/CreditoTest.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+namespace DomainTest
+{
+	[TestClass]
+	public class CreditoTest
+	{
+		[TestMethod]
+		public void Nueva_Cuenta_Credito_No_Nula()
+		{
+			Credito cuentaCredito = new Credito();
+			Assert.IsNotNull(cuentaCredito);
+		}
+	}
+}
