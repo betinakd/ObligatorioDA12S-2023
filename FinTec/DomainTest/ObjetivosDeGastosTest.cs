86fc3a5e157392ef08efb774e966bdd62b53764a
maxxi98 <maxi_nos_10@hotmail.com>
2023-09-29 23:08:31 -0300

    RED: Nuevo_Objetivo

---
+++ FinTec/DomainTest/ObjetivosDeGastosTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DomainTest
+{
+    [TestClass]
+    public class ObjetivosDeGastosTest
+    {
+        [TestMethod]
+        public void Nuevo_Objetivo()
+        {
+            var objetivo = new ObjetivosDeGastos();
+            Assert.IsNotNull(objetivo);
+        }
+    }
+}
