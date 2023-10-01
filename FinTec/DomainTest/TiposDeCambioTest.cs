85faf8be2a31e190ab16cdc3eb99bd695fbb64be
maxxi98 <maxi_nos_10@hotmail.com>
2023-09-30 21:27:22 -0300

    RED: Nuevo_TipoDeCambio

---
+++ FinTec/DomainTest/TiposDeCambioTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DomainTest
+{
+    [TestClass]
+    public class TiposDeCambioTest
+    {
+        [TestMethod]
+        public void Nuevo_TipoDeCambio()
+        {
+            var tipoDeCambio = new TiposDeCambio();
+            Assert.IsNotNull(tipoDeCambio);
+        }
+    }
+}
