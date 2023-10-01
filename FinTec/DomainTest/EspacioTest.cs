7b69c061313fc5661418aa7aaa02218abb5dbddd
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-10-01 16:17:58 -0300

    RED: Cambiar_Admin_Y_Verificar_ListaDeUsuarios

--- FinTec/DomainTest/EspacioTest.cs
+++ FinTec/DomainTest/EspacioTest.cs
@@ -13,31 +13,31 @@ namespace DomainTest
 
 
 		[TestMethod]
-		public void Espacio_Tiene_Admin() 
+		public void Espacio_Tiene_Admin()
 		{
 			var espacio = new Espacio();
 			Usuario usuario = new Usuario();
 			espacio.Admin = usuario;
-			Assert.AreEqual(espacio.Admin,usuario);
+			Assert.AreEqual(espacio.Admin, usuario);
 		}
 
 		[TestMethod]
 		[ExpectedException(typeof(DomainEspacioException))]
 		public void Excepcion_Espacio_Tiene_Admin_Nulo()
-		{			
+		{
 			var espacio = new Espacio();
 			espacio.Admin = null;
 		}
 
 		[TestMethod]
-		public void Espacio_Tiene_UsuariosInvitados() 
+		public void Espacio_Tiene_UsuariosInvitados()
 		{
 			var espacio = new Espacio();
 			Usuario usuario = new Usuario();
 			List<Usuario> usuarios = new List<Usuario>();
 			espacio.Admin = usuario;
 			espacio.UsuariosInvitados = usuarios;
-			Assert.AreEqual(espacio.UsuariosInvitados,usuarios);
+			Assert.AreEqual(espacio.UsuariosInvitados, usuarios);
 		}
 
 		[TestMethod]
@@ -50,6 +50,26 @@ namespace DomainTest
 			espacio.Admin = usuario;
 			usuarios.Add(usuario);
 			espacio.UsuariosInvitados = usuarios;
+		}
+
+		[TestMethod]
+		public void Cambiar_Admin_Y_Verificar_ListaDeUsuarios()
+		{
+			var espacio = new Espacio();
+			Usuario usuario = new Usuario();
+			Usuario usuario2 = new Usuario();
+			List<Usuario> usuarios = new List<Usuario>();
+
+			// Creamos lista de usuarios invitados con el usuario2 incluido
+			usuarios.Add(usuario2);
+			espacio.UsuariosInvitados = usuarios;
+			// Hacemos admin al usuario
+			espacio.Admin = usuario;
+			// Cambiamos de admin a usuario2 por lo que usuario deberia estar en la lista de invitados y usuario2 no
+			espacio.cambiarAdmin(usuario2);
+			Assert.AreEqual(espacio.Admin, usuario2);
+			CollectionAssert.DoesNotContain(espacio.UsuariosInvitados, usuario2);
+			CollectionAssert.Contains(espacio.UsuariosInvitados, usuario);
 		}
 	}
 }
