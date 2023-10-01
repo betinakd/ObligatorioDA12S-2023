630628a1430567f778bb093626d188daac04e7c1
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-10-01 15:46:10 -0300

    RED: Espacio_Tiene_Admin

--- FinTec/DomainTest/EspacioTest.cs
+++ FinTec/DomainTest/EspacioTest.cs
@@ -10,5 +10,15 @@ namespace DomainTest
 			var espacio = new Espacio();
 			Assert.IsNotNull(espacio);
 		}
+
+
+		[TestMethod]
+		public void Espacio_Tiene_Admin() 
+		{
+			var espacio = new Espacio();
+			Usuario usuario = new Usuario();
+			espacio.Admin = usuario;
+			Assert.AreEqual(espacio.Admin,usuario);
+		}
 	}
 }
