1c50db4581a802c41dbba8170611f4c00949c99a
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-10-01 18:03:58 -0300

    GREEN: Espacio_Inicializa_Lista_Categorias_Vacia

--- FinTec/Domain/Espacio.cs
+++ FinTec/Domain/Espacio.cs
@@ -4,8 +4,15 @@
 	{
 		private Usuario _admin;
 		private List<Cuenta> _cuentas = new List<Cuenta>();
+		private List <Categoria> _categorias = new List<Categoria>();
 		private List<Usuario> _usuariosInvitados = new List<Usuario>();
-
+		public List<Categoria> Categorias
+		{
+			get
+			{
+				return _categorias;
+			}
+		}
 		public List<Cuenta> Cuentas
 		{
 			get
