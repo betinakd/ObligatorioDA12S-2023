5b52b1161096027f01aef92f1e338eec3548e9fa
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-10-01 15:38:55 -0300

    RED: Nuevo_Espacio_No_Nulo

---
+++ FinTec/DomainTest/EspacioTest.cs
@@ -0,0 +1,13 @@
+﻿namespace DomainTest
+{
+	[TestClass]
+	public class EspacioTest
+	{
+		[TestMethod]
+		public void Nueva_Espacio_No_Nulo()
+		{
+			var espacio = new Espacio();
+			Assert.IsNotNull(espacio);
+		}
+	}
+}
