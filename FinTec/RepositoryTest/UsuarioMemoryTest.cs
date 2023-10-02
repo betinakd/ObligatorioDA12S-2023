e299ecf136df43e00fa001075bee1edf3bcbec1f
maxxi98 <maxi_nos_10@hotmail.com>
2023-10-02 18:27:59 -0300

    RED : Actualizar_Usuario

--- FinTec/RepositoryTest/UsuarioMemoryTest.cs
+++ FinTec/RepositoryTest/UsuarioMemoryTest.cs
@@ -45,5 +45,21 @@ namespace RepositoryTest
             var usuarioAgregado1 = repository.Add(usuario1);
             var usuarioAgregado2 = repository.Add(usuario2);
         }
+
+        [TestMethod]
+        public void Actualizar_Usuario()
+        {
+            var usuario1 = new Usuario
+            {
+                Correo = "Juan@xxxx.com",
+                Contrasena = "123456",
+            };
+            var repository = new UsuarioMemoryRepository();
+            var usuarioAgregado1 = repository.Add(usuario1);
+            usuario1.Contrasena = "1234567";
+            var usuarioAgregado2 = repository.Update(usuario1);
+            Assert.IsNotNull(usuarioAgregado2);
+            Assert.AreEqual("1234567", usuarioAgregado2.Contrasena);
+        }
     }
 }
