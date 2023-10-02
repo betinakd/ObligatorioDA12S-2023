764bee972428b22377cc9fa3ecc9e4a2704beb63
maxxi98 <maxi_nos_10@hotmail.com>
2023-10-02 18:33:27 -0300

    GREEN: Buscar_Usuario

--- FinTec/RepositoryTest/UsuarioMemoryTest.cs
+++ FinTec/RepositoryTest/UsuarioMemoryTest.cs
@@ -76,5 +76,20 @@ namespace RepositoryTest
             var usuarioAgregado2 = repository.Find(u => u.Correo == usuario1.Correo);
             Assert.IsNull(usuarioAgregado2);
         }
+
+        [TestMethod]
+        public void Buscar_Usuario()
+        {
+            var usuario1 = new Usuario
+            {
+                Correo = "Juan@xxxx.com",
+                Contrasena = "123456",
+            };
+            var repository = new UsuarioMemoryRepository();
+            var usuarioAgregado1 = repository.Add(usuario1);
+            var usuarioAgregado2 = repository.Find(u => u.Correo == usuario1.Correo);
+            Assert.IsNotNull(usuarioAgregado2);
+            Assert.AreEqual(usuario1, usuarioAgregado2);
+        }
     }
 }
