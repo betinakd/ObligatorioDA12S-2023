3835e9ec94de31c282364466e2ce16f99c1df360
maxxi98 <maxi_nos_10@hotmail.com>
2023-10-03 14:55:19 -0300

    Borro todo de TransaccionIngreso y de su test

--- FinTec/Domain/TransaccionIngreso.cs
+++ FinTec/Domain/TransaccionIngreso.cs
@@ -8,59 +8,5 @@ namespace Domain
 {
     public class TransaccionIngreso : Transaccion
     {
-        private string _titulo;
-        public override string Titulo
-        {
-            get
-            {
-                return _titulo;
-            }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                    throw new DomainTransaccionException("El titulo no puede ser vacio");
-                _titulo = value;
-            }
-        }
-
-        private readonly DateTime _fechaCreacion = DateTime.Now;
-        public override DateTime FechaTransaccion
-        {
-            get { return _fechaCreacion; }
-        }
-
-        private double _monto;
-        public override double Monto
-        {
-            get
-            {
-                return _monto;
-            }
-            set
-            {
-                if (value <= 0)
-                    throw new DomainTransaccionException("El monto debe ser mayor a cero");
-                _monto = value;
-            }
-        }
-
-        public override TipoCambiario Moneda { get; set; }
-
-        public override Cuenta CuentaMonetaria { get; set; }
-
-        private Categoria _categoria;
-        public override Categoria CategoriaTransaccion
-        {
-            get
-            {
-                return _categoria;
-            }
-            set
-            {
-                if (value.EstadoActivo == false)
-                    throw new DomainTransaccionException("La categoria tiene que estar activa");
-                _categoria = value;
-            }
-        }
     }
 }
