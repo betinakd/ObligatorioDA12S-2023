3262ebc8fbf4748de5d30b06c7c21eead68cdc4b
maxxi98 <maxi_nos_10@hotmail.com>
2023-10-03 18:16:37 -0300

    RED: Nuevo_UsuarioLogic

---
+++ FinTec/BussinesLogic/UsuarioLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Repository;
+
+namespace BussinesLogic
+{
+    public class UsuarioLogic
+    {
+    }
+}
