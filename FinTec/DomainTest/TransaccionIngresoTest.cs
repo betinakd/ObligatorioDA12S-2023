acf27c2b3f927e0d7109b5c652adce8a4444f714
maxxi98 <maxi_nos_10@hotmail.com>
2023-10-03 15:01:16 -0300

    RED: Tipo_Categoria_Distinto_TransaccionI

--- FinTec/DomainTest/TransaccionIngresoTest.cs
+++ FinTec/DomainTest/TransaccionIngresoTest.cs
@@ -10,5 +10,16 @@ namespace DomainTest
     [TestClass]
     public class TransaccionIngresoTest
     {
+        [TestMethod]
+        [ExpectedException(typeof(DomainTransaccionException))]
+        public void Tipo_Categoria_Distinto_TransaccionI()
+        {
+            var transaccion = new TransaccionIngreso();
+            Categoria categoria = new Categoria();
+            categoria.Nombre = "Categoria1";
+            categoria.Tipo = TipoCategoria.Costo;
+            categoria.EstadoActivo = true;
+            transaccion.CategoriaTransaccion = categoria;
+        }
     }
 }
