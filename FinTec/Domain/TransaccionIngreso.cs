871dbf482e3bbf3317e967f3953c45d0a92f5d0a
maxxi98 <maxi_nos_10@hotmail.com>
2023-10-03 15:02:15 -0300

    GREEN: Tipo_Categoria_Distinto_TransaccionI

--- FinTec/Domain/TransaccionIngreso.cs
+++ FinTec/Domain/TransaccionIngreso.cs
@@ -8,5 +8,21 @@ namespace Domain
 {
     public class TransaccionIngreso : Transaccion
     {
+        private Categoria _categoria;
+        public override Categoria CategoriaTransaccion
+        {
+            get
+            {
+                return _categoria;
+            }
+            set
+            {
+                if (value.EstadoActivo == false)
+                    throw new DomainTransaccionException("La categoria tiene que estar activa");
+                if (value.Tipo != TipoCategoria.Ingreso)
+                    throw new DomainTransaccionException("La categoria tiene que ser de tipo Ingreso");
+                _categoria = value;
+            }
+        }
     }
 }
