1e470ef905fee83b81f38cd58ae1c45acd62521d
Mateo Arias <mateoarias437@gmail.com>
2023-10-05 21:35:52 -0300

    RED: Test Reporte_No_Nulo

---
+++ FinTec/Domain/Reporte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Reporte
+    {
+        private int _contadorId = 1;
+        private int _id;
+        private Espacio? _MiEspacio;
+
+        public Espacio MiEspacio
+        {
+            set { _MiEspacio = value; }
+            get { return _MiEspacio; }
+        }
+    }
+}
---
+++ FinTec/DomainTest/ReporteTest.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+
+namespace DomainTest
+{
+    [TestClass]
+    public class ReporteTest
+    {
+        [TestMethod]
+        public void Reporte_No_Nulo()
+        {
+            var rep1 = new Reporte();
+
+            Assert.IsNull(rep1);
+        }
+    }
+}
