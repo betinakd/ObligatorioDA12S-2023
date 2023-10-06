1cdbd5329a759a2758571cd156d91d6dc00d3c02
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-10-05 23:09:05 -0300

    GREEN: Retorna_Lista_Espacios_Recibiendo_Correo_Valido

--- FinTec/BussinesLogic/EspacioLogic.cs
+++ FinTec/BussinesLogic/EspacioLogic.cs
@@ -28,8 +28,20 @@ namespace BussinesLogic
             _repository.Add(oneElement);
             return oneElement;
         }
-
-        public void DeleteEspacio(Espacio oneElement)
+		public List<Espacio> EspaciosByCorreo(string correo)
+		{
+			IList<Espacio> espacios = _repository.FindAll();
+			List<Espacio> espaciosUsuario = new List<Espacio>();
+			foreach (Espacio espacio in espacios)
+			{
+				if (espacio.PerteneceCorreo(correo))
+				{
+					espaciosUsuario.Add(espacio);
+				}
+			}
+			return espaciosUsuario;
+		}
+		public void DeleteEspacio(Espacio oneElement)
         {
             _repository.Delete(oneElement.Admin.Correo);
         }
@@ -43,5 +55,7 @@ namespace BussinesLogic
         {
             return _repository.Find(u => u.Admin.Correo == id);
         }
+
+
     }
 }
