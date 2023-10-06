738ab2cf90de87a774901da05124645bd81a044c
Mateo Arias <mateoarias437@gmail.com>
2023-10-05 23:25:02 -0300

    RED: Test ObjetivoGasto_No_Vacio_Dos_Atributos

--- FinTec/DomainTest/ObjetivoGastoTest.cs
+++ FinTec/DomainTest/ObjetivoGastoTest.cs
@@ -37,5 +37,14 @@ namespace DomainTest
             var _objetivoGasto = new ObjetivoGasto(_valorEsperado);
             Assert.IsNotNull(_objetivoGasto);
         }
+
+        [TestMethod]
+        public void ObjetivoGasto_No_Vacio_Dos_Atributos()
+        {
+            double valorEsperado = 5;
+            double valorAcumulado = 7;
+            var _objetivoGasto = new ObjetivoGasto(valorEsperado, valorAcumulado);
+            Assert.IsNull(_objetivoGasto); 
+        }
     }
 }
