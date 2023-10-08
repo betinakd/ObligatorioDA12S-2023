7dec87ccb9eb6eb8e03a5082fad57f0f5b0f96b4
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-10-08 18:01:17 -0300

    REFACTOR: Test Initialize

--- FinTec/DomainTest/ObjetivoTest.cs
+++ FinTec/DomainTest/ObjetivoTest.cs
@@ -7,53 +7,79 @@ using Domain;
 
 namespace DomainTest
 {
-    [TestClass]
-    public class ObjetivoTest
-    {
-        [TestMethod]
-        public void Nuevo_Objetivo()
-        {
-            var objetivo = new Objetivo();
-            Assert.IsNotNull(objetivo);
-        }
+	[TestClass]
+	public class ObjetivoTest
+	{
+		private Categoria _categoria;
+		private List<Categoria> _categorias = new List<Categoria>();
+		private Objetivo _objetivo;
 
-        [TestMethod]
-        public void Tiene_Titulo()
-        {
-            var objetivo = new Objetivo();
-            objetivo.Titulo = "Objetivo 1";
-            Assert.AreEqual("Objetivo 1", objetivo.Titulo);
-        }
+		[TestInitialize]
+		public void InitTest()
+		{
+			_categoria = new Categoria()
+			{
+				Nombre = "Sueldo",
+				Tipo = TipoCategoria.Ingreso,
+				EstadoActivo = true
+			};
+			_categorias.Add(_categoria);
 
-        [ExpectedException(typeof(ArgumentNullException))]
-        [TestMethod]
-        public void Tiene_Titulo_Vacio()
-        {
-            var objetivo = new Objetivo();
-            objetivo.Titulo = "";
-        }
+			_objetivo = new Objetivo()
+			{
+				Titulo = "Objetivo 1",
+				MontoMaximo = 1000,
+				Categorias = _categorias,
+			};
+		}
 
-        [TestMethod]
-        public void Monto_Maximo()
-        {
-            var objetivo = new Objetivo();
-            objetivo.MontoMaximo = 1000;
-            Assert.AreEqual(1000, objetivo.MontoMaximo);
-        }
+		[TestMethod]
+		public void Nuevo_Objetivo()
 
-        [TestMethod]
-        public void  Aplicar_Ojetivo_Categoria()
-        {
-            var objetivo = new Objetivo();
-            var categoria = new Categoria();
-            var categorias = new List<Categoria>();
-            categoria.EstadoActivo=true;
-            categoria.Tipo=TipoCategoria.Ingreso;
-            categoria.Nombre="Sueldo";
-            categorias.Add(categoria);
-            objetivo.Categorias=categorias;
-            var resultado = objetivo.Categorias;
-            Assert.AreEqual(categorias, resultado);
-        }
-    }
+		{
+			var objetivo = new Objetivo();
+			Assert.IsNotNull(objetivo);
+		}
+
+		[TestMethod]
+		public void Tiene_Titulo()
+		{
+			_objetivo.Titulo = "Objetivo 1";
+			Assert.AreEqual("Objetivo 1", _objetivo.Titulo);
+		}
+
+		[ExpectedException(typeof(ArgumentNullException))]
+		[TestMethod]
+		public void Tiene_Titulo_Vacio()
+
+		{
+			_objetivo.Titulo = "";
+		}
+
+		[TestMethod]
+		public void Monto_Maximo()
+
+		{
+			_objetivo.MontoMaximo = 1000;
+			Assert.AreEqual(1000, _objetivo.MontoMaximo);
+		}
+
+		[TestMethod]
+		public void Aplicar_Ojetivo_Categoria()
+
+		{
+			var objetivo = new Objetivo();
+			var categoria = new Categoria();
+			var categorias = new List<Categoria>();
+			categoria.EstadoActivo = true;
+			categoria.Tipo = TipoCategoria.Ingreso;
+			categoria.Nombre = "Sueldo";
+			categorias.Add(categoria);
+			objetivo.Categorias = categorias;
+			var resultado = objetivo.Categorias;
+			Assert.AreEqual(categorias, resultado);
+		}
+
+	
+	}
 }
