2c4cf8ee6b941ab5b2ffa6f4626e0112dee34791
Mateo Arias <mateoarias437@gmail.com>
2023-10-08 15:41:06 -0300

    RED: Test ReporteObjetivosGastos_No_Vacio

--- FinTec/DomainTest/ReporteTest.cs
+++ FinTec/DomainTest/ReporteTest.cs
@@ -34,10 +34,52 @@ namespace DomainTest
             _miEspacio.Admin = _admin;
             var _reporte = new Reporte();
             _reporte.MiEspacio = _miEspacio;
-            //List<ObjetivoGasto> _objGastoVacio = new List<ObjetivoGasto>();
             List<ObjetivoGasto> _reporteGastos = _reporte.ReporteObjetivosDeGastos();
             Assert.IsTrue(_reporteGastos.Count == 0);
         }
 
+        [TestMethod]
+        public void ReporteObjetivosGastos_No_Vacio()
+        {
+            var _reporte = new Reporte();
+            Espacio _miEspacio = new Espacio();
+            Usuario _admin = new Usuario
+            {
+                Contrasena = "1234567890Yuu",
+                Correo = "mateo@gmail.com",
+            };
+            _miEspacio.Admin = _admin;
+            Categoria _categoria = new Categoria
+            {
+                EstadoActivo = true,
+                Tipo = TipoCategoria.Costo,
+                Nombre = "Una categoria",
+            };
+            _miEspacio.AgregarCategoria(_categoria);
+            Cuenta _cuenta = new Cuenta();
+            _miEspacio.AgregarCuenta(_cuenta);
+            Transaccion transaccion = new Transaccion
+            {
+                CategoriaTransaccion = _categoria,
+                Monto = 10,
+                Moneda = TipoCambiario.PesosUruguayos,
+                Titulo = "Transaccion Prueba",
+                CuentaMonetaria = _cuenta,
+            };
+            _miEspacio.AgregarTransaccion(transaccion);
+            List<Categoria> _listCat = new List<Categoria>();
+            _listCat.Add(_categoria);
+            Objetivo _objetivo = new Objetivo
+            {
+                Categorias = _listCat,
+                MontoMaximo = 100,
+                Titulo = "Menos gastos",
+            };
+            _miEspacio.AgregarObjetivo(_objetivo);
+            _reporte.MiEspacio = _miEspacio;
+            List<ObjetivoGasto> ret = _reporte.ReporteObjetivosDeGastos();
+            List<ObjetivoGasto> ret2 = new List<ObjetivoGasto>();
+            Assert.IsFalse(ret.Count == 1);
+        }
     }
 }
