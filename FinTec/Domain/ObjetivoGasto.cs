649eb425140e71c895f070a31663828f55f3e104
Mateo Arias <mateoarias437@gmail.com>
2023-10-08 16:19:40 -0300

    RED: Test ReporteObjetivoGasto_Monto_No_Cumple

--- FinTec/Domain/ObjetivoGasto.cs
+++ FinTec/Domain/ObjetivoGasto.cs
@@ -33,7 +33,7 @@ namespace Domain
         
         public bool MontoCumpido()
         {
-            return MontoEsperado < MontoAcumulado;
+            return MontoEsperado > MontoAcumulado;
         }
 
         public override bool Equals(object? obj)
