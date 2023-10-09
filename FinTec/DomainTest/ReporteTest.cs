742350d79c81e41a3ff00b53cf10049837920771
Mateo Arias <mateoarias437@gmail.com>
2023-10-09 10:34:28 -0300

    GREEN: TransaccionDentroDelSecope_True

--- FinTec/DomainTest/ReporteTest.cs
+++ FinTec/DomainTest/ReporteTest.cs
@@ -631,7 +631,7 @@ namespace DomainTest
             _miEspacio.AgregarCategoria(_categoriaCosto);
             _reporte.MiEspacio = _miEspacio;
             bool control = _reporte.TransaccionDentroDelScope(transaccion1, DateTime.Now.AddMonths(-5), DateTime.Now.AddMonths(+1));
-            Assert.IsFalse(control);
+            Assert.IsFalse(!control);
         }
 
         [TestMethod]
