612af1009074bf2f7808a2a76848108c5ff076d5
Mateo Arias <mateoarias437@gmail.com>
2023-10-09 10:32:02 -0300

    RED: TransaccionDentroDelScope_False

--- FinTec/DomainTest/ReporteTest.cs
+++ FinTec/DomainTest/ReporteTest.cs
@@ -551,6 +551,48 @@ namespace DomainTest
         }
 
         [TestMethod]
+        public void TransaccionDentroDelScope_False()
+        {
+            var _reporte = new Reporte();
+            Espacio _miEspacio = new Espacio();
+            Usuario _admin = new Usuario
+            {
+                Contrasena = "1234567890Yuu",
+                Correo = "mateo@gmail.com",
+            };
+            _miEspacio.Admin = _admin;
+            Credito credit = new Credito
+            {
+                BancoEmisor = "Santander",
+                CreditoDisponible = 1000,
+                FechaCierre = DateTime.Now.AddMonths(+2),
+                Moneda = TipoCambiario.PesosUruguayos,
+                NumeroTarjeta = "1111",
+            };
+            _miEspacio.AgregarCuenta(credit);
+            Categoria _categoriaCosto = new Categoria
+            {
+                EstadoActivo = true,
+                Tipo = TipoCategoria.Ingreso,
+                Nombre = "Una categoria",
+            };
+            _miEspacio.AgregarCategoria(_categoriaCosto);
+            Transaccion transaccion1 = new Transaccion
+            {
+                CategoriaTransaccion = _categoriaCosto,
+                Monto = 10,
+                Moneda = TipoCambiario.PesosUruguayos,
+                Titulo = "Transaccion Prueba 1",
+                CuentaMonetaria = credit,
+            };
+            _miEspacio.AgregarTransaccion(transaccion1);
+            _miEspacio.AgregarCategoria(_categoriaCosto);
+            _reporte.MiEspacio = _miEspacio;
+            bool control = _reporte.TransaccionDentroDelScope(transaccion1, DateTime.Now.AddMonths(-5), DateTime.Now.AddMonths(-4));
+            Assert.IsTrue(control);
+        }
+
+        [TestMethod]
         public void ReporteCategoriaPorMes_Vacio()
         {
             var _reporte = new Reporte();
