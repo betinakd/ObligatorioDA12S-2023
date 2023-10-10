4fa820c0cc9d206e59c4eb57a81331a60df7158a
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-10-10 10:26:05 -0300

    GREEN: Excepcion_Categoria_Nula

--- FinTec/Domain/TransaccionIngreso.cs
+++ FinTec/Domain/TransaccionIngreso.cs
@@ -17,6 +17,8 @@ namespace Domain
 			}
 			set
 			{
+				if (value == null)
+					throw new DomainEspacioException("La categoria no puede ser nula.");
 				if (value.EstadoActivo == false)
 					throw new DomainEspacioException("La categoria tiene que estar activa");
 				if (value.Tipo != TipoCategoria.Ingreso)
