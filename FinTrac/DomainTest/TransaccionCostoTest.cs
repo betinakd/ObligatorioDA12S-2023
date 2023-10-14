8b44981e1053acedeb25f21e93e9a43aaf4987c5
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-10-14 11:02:10 -0300

    RED: TransaccionCosto_Clon_Cuenta_Credito

--- FinTrac/DomainTest/TransaccionCostoTest.cs
+++ FinTrac/DomainTest/TransaccionCostoTest.cs
@@ -7,22 +7,40 @@ namespace DomainTest
 	public class TransaccionCostoTest
 	{
 		private TransaccionCosto transaccion1;
+		private TransaccionCosto transaccion2;
+		private Ahorro cuenta1;
 
 		[TestInitialize]
 		public void InitTests()
 		{
-			var cuenta = new Ahorro()
+			cuenta1 = new Ahorro()
 			{
 				Nombre = "Cuenta1",
 				Moneda = TipoCambiario.Dolar,
 				Monto = 100,
+			};
+			var cuenta2 = new Credito()
+			{
+				NumeroTarjeta = "1234",
+				BancoEmisor = "BROU",
+				CreditoDisponible = 100,
+				Moneda = TipoCambiario.Dolar,
+				FechaCierre = DateTime.Now.AddDays(4),
 			};
 			transaccion1 = new TransaccionCosto()
 			{
 				Monto = 100,
 				Titulo = "Transaccion1",
 				Moneda = TipoCambiario.Dolar,
-				CuentaMonetaria = cuenta,
+				CuentaMonetaria = cuenta1,
+				FechaTransaccion = DateTime.Today,
+			};
+			transaccion2 = new TransaccionCosto()
+			{
+				Monto = 1090,
+				Titulo = "Transaccion1",
+				Moneda = TipoCambiario.Dolar,
+				CuentaMonetaria = cuenta2,
 				FechaTransaccion = DateTime.Today,
 			};
 		}
@@ -71,7 +89,7 @@ namespace DomainTest
 		}
 
 		[TestMethod]
-		public void TransaccionCosto_Clon_Cuenta_Credito()
+		public void TransaccionCosto_Clon_Cuenta_Ahorro()
 		{
 			var transaccionClon = transaccion1.ClonTransaccion(transaccion1);
 			Assert.AreEqual(transaccion1.Titulo, transaccionClon.Titulo);
@@ -79,6 +97,17 @@ namespace DomainTest
 			Assert.AreEqual(transaccion1.CuentaMonetaria, transaccionClon.CuentaMonetaria);
 			Assert.AreEqual(transaccion1.CategoriaTransaccion, transaccionClon.CategoriaTransaccion);
 			Assert.AreEqual(transaccion1.Monto, transaccionClon.Monto);
+		}
+
+		[TestMethod]
+		public void TransaccionCosto_Clon_Cuenta_Credito()
+		{
+			var transaccionClon = transaccion2.ClonTransaccion(transaccion2);
+			Assert.AreEqual(transaccion2.Titulo, transaccionClon.Titulo);
+			Assert.AreEqual(transaccion2.Moneda, transaccionClon.Moneda);
+			Assert.AreEqual(transaccion2.CuentaMonetaria, transaccionClon.CuentaMonetaria);
+			Assert.AreEqual(transaccion2.CategoriaTransaccion, transaccionClon.CategoriaTransaccion);
+			Assert.AreEqual(transaccion2.Monto, transaccionClon.Monto);
 		}
 
 		[TestMethod]
