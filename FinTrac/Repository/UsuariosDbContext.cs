cea7958cc9fe4093325c37b11595c66916dd7c93
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-10-27 03:14:59 -0300

    add: usuariosDbContext.cs

---
+++ FinTrac/Repository/UsuariosDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Repository
+{
+	public class UsuariosDbContext : DbContext
+	{
+		public DbSet<Usuario> Usuarios { get; set; }
+
+		public UsuariosDbContext(DbContextOptions<UsuariosDbContext> options) : base(options)
+		{
+		}
+
+		//protected override void OnModelCreating(ModelBuilder modelBuilder)
+		//{
+		//	modelBuilder.Entity<Usuario>().ToTable("Usuarios");
+		//}
+	}
+}
