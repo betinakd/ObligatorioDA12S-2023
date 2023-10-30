9706617afb475a3d359c869575ae90bacfee9b00
maxxi98 <maxi_nos_10@hotmail.com>
2023-10-30 13:50:21 -0300

    GREEN: DB_Get_Usuario

--- FinTrac/Repository/UsuariosDbContext.cs
+++ FinTrac/Repository/UsuariosDbContext.cs
@@ -5,5 +5,48 @@ namespace Repository
 {
 	public class UsuariosDbContext : DbContext
 	{
+		public DbSet<Usuario> Usuarios { get; set; }
+		public UsuariosDbContext(DbContextOptions<UsuariosDbContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+			modelBuilder.Entity<Usuario>()
+				.HasKey(u => u.Correo);
+
+			modelBuilder.Entity<Espacio>()
+				.HasMany(e => e.UsuariosInvitados);
+			
+			modelBuilder.Entity<Espacio>()
+				.HasKey(e => e.Id);
+
+			modelBuilder.Entity<Espacio>()
+				.HasMany(c => c.Cambios);
+
+			modelBuilder.Entity<Cambio>()
+					.HasKey(c => new
+					{
+						c.FechaDeCambio,
+						c.Moneda,
+						
+					});
+
+			modelBuilder.Entity<Categoria>()
+				.HasKey(c => c.Nombre);
+
+			modelBuilder.Entity<Cuenta>()
+						.HasKey(c => c.FechaCreacion);
+
+			modelBuilder.Entity<Transaccion>()
+						.HasKey(t => t.IdTransaccion);
+
+			modelBuilder.Entity<Objetivo>()
+						.HasKey(o => o.Titulo);
+
+			modelBuilder.Entity<Objetivo>()
+						.HasMany(o => o.Categorias);
+		}
 	}
 }
