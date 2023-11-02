3e8f7600b6f02a6baa23942b884bcd7c62b20d39
maxxi98 <maxi_nos_10@hotmail.com>
2023-11-02 12:40:21 -0300

    REFACTOR: EspacioLogicTest

--- FinTrac/BussinesLogicTest/EspacioLogicTest.cs
+++ FinTrac/BussinesLogicTest/EspacioLogicTest.cs
@@ -9,36 +9,59 @@ namespace BussinesLogicTest
     [TestClass]
     public class EspacioLogicTest
     {
-        private IRepository<Espacio> repository;
+        private IRepository<Espacio> _repository;
         private EspacioLogic espacioLogic;
         private Espacio espacio1;
         private Espacio espacio2;
-		private readonly UsuariosDbContext _context;
+		private UsuariosDbContext _context;
+		private readonly IDbContextFactory _contextFactory = new InMemoryDbContextFactory();
+        Usuario usuario1;
 
 		[TestInitialize]
         public void Setup()
         {
-            repository = new EspacioMemoryRepository(_context);
-            espacioLogic = new EspacioLogic(repository);
+			_context = _contextFactory.CreateDbContext();
+			_repository = new EspacioMemoryRepository(_context);
+            espacioLogic = new EspacioLogic(_repository);
+
+            usuario1 = new Usuario()
+            {
+                Nombre = "Maxi",
+                Apellido = "Gimenez",
+                Direccion = "address",
+                Correo = "xx@yy.com",
+                Contrasena = "123456789A",
+            };
+
             espacio1 = new Espacio()
             {
-                Admin = new Usuario()
-                {
-                    Correo = "xx@yy.com",
-                    Contrasena = "123456789A",
-                }
+                Nombre = "Espacio1",
+                Admin = usuario1  
             };
 
             espacio2 = new Espacio()
             { 
+                Nombre = "Espacio2",
                 Admin = new Usuario()
                 {
+                    Nombre = "Max",
+                    Apellido = "Gim",
+                    Direccion = "direccion",
                     Correo = "xxxx@yyyy.com",
                     Contrasena = "123456789B",
                 }
             };
         }
 
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+            _context = null;
+			_repository = null;
+			espacioLogic = null;
+		}
 
         [TestMethod]
         public void Nuevo_EspacioLogic()
@@ -50,7 +73,7 @@ namespace BussinesLogicTest
         public void Agregar_Espacio()
         {
             espacioLogic.AddEspacio(espacio1);
-            Assert.IsTrue(repository.FindAll().Contains(espacio1));
+			Assert.IsTrue(_repository.FindAll().Contains(espacio1));
         }
 
         [TestMethod]
@@ -66,7 +89,7 @@ namespace BussinesLogicTest
         {
             espacioLogic.AddEspacio(espacio1);
             espacioLogic.DeleteEspacio(espacio1);
-            Assert.IsFalse(repository.FindAll().Contains(espacio1));
+            Assert.IsFalse(_repository.FindAll().Contains(espacio1));
         }
 
         [TestMethod]
@@ -81,11 +104,20 @@ namespace BussinesLogicTest
         [TestMethod]
         public void Buscar_Espacio()
         {
+            espacioLogic.AddEspacio(espacio1);
+            espacioLogic.AddEspacio(espacio2);
             Espacio espacio = new Espacio();
+            espacio.Nombre = "Espacio";
+            espacio.Admin = new Usuario()
+            {
+                Nombre = "Maximiliano",
+                Apellido = "Gimenezz",
+                Direccion = "address2",
+                Correo = "c@c.com",
+                Contrasena = "123456789C",
+            };
             espacioLogic.AddEspacio(espacio);
-
             Espacio resultado1 = espacioLogic.FindEspacio(espacio.Id);
-
 			Assert.AreEqual(espacio.Nombre, resultado1.Nombre);
         }
 
@@ -93,8 +125,12 @@ namespace BussinesLogicTest
         public void Retorna_Lista_Espacios_Recibiendo_Correo_Valido() { 
             espacioLogic.AddEspacio(espacio1);
 			espacioLogic.AddEspacio(espacio2);
-            List<Espacio> espacios = espacioLogic.EspaciosByCorreo("xx@yy.com");
-            Assert.IsTrue(espacios.Count == 1);
+			Espacio espacio = new Espacio();
+			espacio.Nombre = "Espacio";
+            espacio.Admin = usuario1;
+            espacioLogic.AddEspacio(espacio);
+			List<Espacio> espacios = espacioLogic.EspaciosByCorreo("xx@yy.com");
+            Assert.IsTrue(espacios.Count == 2);
         }
     }
 }
