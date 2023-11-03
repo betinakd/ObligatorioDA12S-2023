8b59b4f3c0b46933d37aac30dda3ec6734cd3ffd
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-03 12:33:34 -0300

    GREEN: Usuario_Tiene_Lista_Espacios

--- FinTrac/Domain/Usuario.cs
+++ FinTrac/Domain/Usuario.cs
@@ -5,6 +5,7 @@ namespace Domain
 {
 	public class Usuario
 	{
+		public List<Espacio> Espacios { get; set; }
 		public int IdEspacioPrincipal { get; set; }
 		private string _contrasena;
 		public string Direccion { get; set; }
