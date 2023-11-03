d726ae783cd5ba336e400989cdc206b168bbebe8
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-03 09:56:30 -0300

    Refactor Usuarios tests

--- FinTrac/RepositoryTest/UsuarioMemoryTest.cs
+++ FinTrac/RepositoryTest/UsuarioMemoryTest.cs
@@ -1,82 +1,49 @@
-using Domain;
 using Repository;
+using Domain;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore;
+
 namespace RepositoryTest
 {
-    [TestClass]
-    public class UsuarioMemoryTest
-    {
-        [TestMethod]
-        public void Agregar_Usuario()
-        {
-            var usuario1 = new Usuario
-            {
-                Correo = "Juan@xxxx.com",
-                Contrasena = "123456Yuuuu",
-            };
-            var usuario2 = new Usuario
-            {
-                Correo = "Juann2@xxxx.com",
-                Contrasena = "123456Yuuuuu",
-            };
-            var repository = new UsuarioMemoryRepository();
-            var usuarioAgregado1 = repository.Add(usuario1);
-            var usuarioAgregado2 = repository.Add(usuario2);
-            Assert.IsNotNull(usuarioAgregado1);
-            Assert.AreEqual(usuario1, usuarioAgregado1);
-            Assert.IsNotNull(usuarioAgregado2);
-            Assert.AreEqual(usuario2, usuarioAgregado2);   
-        }
+	[TestClass]
+	public class UsuarioMemoryRepositoryTest
+	{
+		private UsuarioMemoryRepository _repository;
+		private FintracDbContext _context;
+		private readonly IDbContextFactory _contextFactory = new InMemoryDbContextFactory();
 
-        [TestMethod]
-        public void Actualizar_Usuario()
-        {
-            var usuario1 = new Usuario
-            {
-                Correo = "Juan@xxxx.com",
-                Contrasena = "123456Yuuuuuuuu",
-            };
-            var repository = new UsuarioMemoryRepository();
-            var usuarioAgregado1 = repository.Add(usuario1);
-            usuario1.Contrasena = "1234567Yuuuuui";
-            var usuarioAgregado2 = repository.Update(usuario1);
-            Assert.IsNotNull(usuarioAgregado2);
-            Assert.AreEqual("1234567Yuuuuui", usuarioAgregado2.Contrasena);
-        }
+		[TestInitialize]
+		public void SetUp()
+		{
+			_context = _contextFactory.CreateDbContext();
+			_repository = new UsuarioMemoryRepository(_context);
+		}
 
-        [TestMethod]
-        public void Buscar_Usuario()
-        {
-            var usuario1 = new Usuario
-            {
-                Correo = "Juan@xxxx.com",
-                Contrasena = "123456Yuuuuuuuuuuu",
-            };
-            var repository = new UsuarioMemoryRepository();
-            var usuarioAgregado1 = repository.Add(usuario1);
-            var usuarioAgregado2 = repository.Find(u => u.Correo == usuario1.Correo);
-            Assert.IsNotNull(usuarioAgregado2);
-            Assert.AreEqual(usuario1, usuarioAgregado2);
-        }
+		[TestCleanup]
+		public void CleanUp()
+		{
+			_context.Database.EnsureDeleted();
+		}
 
-        [TestMethod]
-        public void Buscar_Todos_Usuarios()
-        {
-            var usuario1 = new Usuario
-            {
-                Correo = "Juan@xxxx.com",
-                Contrasena = "123456Yuuuuuuuu",
-            };
-            var usuario2 = new Usuario
-            {
-                Correo = "Jose@xxxxx.com",
-                Contrasena = "1234567Yuuuuuu",
-            };
-            var repository = new UsuarioMemoryRepository();
-            var usuarioAgregado1 = repository.Add(usuario1);
-            var usuarioAgregado2 = repository.Add(usuario2);
-            var usuarios = repository.FindAll();
-            Assert.IsNotNull(usuarios);
-            Assert.AreEqual(2, usuarios.Count);
-        }
-    }
+		[TestMethod]
+		public void FindAll_Trae_Todos_Los_Usuarios()
+		{
+			var usuario = new Usuario
+			{
+				IdEspacioPrincipal = 1,
+				Nombre = "Usuario1",
+				Apellido = "Apellido1",
+				Correo = "usuario@gmail.com",
+				Contrasena = "HOLAhola123",
+				Direccion = "Direccion1",
+			};
+			_context.Usuarios.Add(usuario);
+			_context.SaveChanges();
+
+			var usuarios = _repository.FindAll();
+			Assert.IsTrue(usuarios.Contains(usuario));
+			Assert.AreEqual(1, usuarios.Count);
+		}
+
+	}
 }
