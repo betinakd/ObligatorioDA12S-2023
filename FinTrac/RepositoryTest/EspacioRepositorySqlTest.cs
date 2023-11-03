d7a83dc9d810196540d8fc498c3c623868f0f948
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-03 06:42:05 -0300

    RED: FindAll_Trae_Todos_Los_Espacios()

--- FinTrac/RepositoryTest/EspacioRepositorySqlTest.cs
+++ FinTrac/RepositoryTest/EspacioRepositorySqlTest.cs
@@ -24,5 +24,30 @@ namespace RepositoryTest
 		{
 			_context.Database.EnsureDeleted();
 		}
+
+		[TestMethod]
+		public void FindAll_Trae_Todos_Los_Espacios()
+		{
+			var espacio = new Espacio
+			{
+				Id = 1,
+				Nombre = "Espacio 1",
+				Admin = new Usuario
+				{
+					IdEspacioPrincipal = 1,
+					Nombre = "Usuario1",
+					Apellido = "Apellido1",
+					Correo = "usuario@gmail.com",
+					Contrasena = "HOLAhola123",
+					Direccion = "Direccion1",
+				}
+			};
+			_context.Espacios.Add(espacio);
+			_context.SaveChanges();
+
+			var espacios = _repository.FindAll();
+			Assert.IsTrue(espacios.Contains(espacio));
+			Assert.AreEqual(1, espacios.Count);
+		}
 	}
 }
