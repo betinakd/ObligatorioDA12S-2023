a99932e7f28cc9a284a80e19c4490d5f8fc535d1
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-04 11:46:03 -0300

    GREEN: Usuario_Tiene_Id

--- FinTrac/Domain/Usuario.cs
+++ FinTrac/Domain/Usuario.cs
@@ -5,6 +5,7 @@ namespace Domain
 {
 	public class Usuario
 	{
+		public int Id { get; set; }
 		public int IdEspacioPrincipal { get; set; }
 		private string _contrasena;
 		public string Direccion { get; set; }
