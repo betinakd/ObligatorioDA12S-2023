669b6836315ff0e9968ae7415f236569f5efe90e
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-05 06:13:54 -0300

    GREEN: ControladorHome_Tiene_Contrasena

--- FinTrac/Controladores/ControladorHome.cs
+++ FinTrac/Controladores/ControladorHome.cs
@@ -5,9 +5,10 @@ namespace Controlador
 	public class ControladorHome
 	{
 		public UsuarioLogic UsuarioLogic { get; set; }
+		public string Correo { get; set; }
 		public string Nombre { get; set; }
 		public string Apellido { get; set; }
 		public string Direccion { get; set; }
-		public string Correo { get; set; }
+		public string Contrasena { get; set; }	
 	}
 }
