c9e671b232c674088f825802f30eca9f76e90d5b
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-05 07:39:32 -0300

    RED: Modificar_Datos_Usuario_Debe_Modificar_Apellido

--- FinTrac/BussinesLogicTest/UsuarioLogicTest.cs
+++ FinTrac/BussinesLogicTest/UsuarioLogicTest.cs
@@ -223,5 +223,24 @@ namespace BussinesLogicTest
 			Usuario usuarioModificado = _usuarioLogic.FindUsuario(usuario.Correo);
 			Assert.AreEqual("Juan", usuarioModificado.Nombre);
 		}
+
+		[TestMethod]
+		public void Modificar_Datos_Usuario_Debe_Modificar_Apellido()
+		{
+			Usuario usuario = new Usuario()
+			{
+				Correo = "alberto@gmail.com",
+				Nombre = "Alberto",
+				Apellido = "Lopez",
+				Contrasena = "HOLAhola123",
+				Direccion = "Bv España 5566"
+			};
+			_usuarioLogic.AddUsuario(usuario);
+
+			_usuarioLogic.ModificarDatosUsuario(usuario.Correo, usuario.Nombre, "Perez", usuario.Contrasena, usuario.Direccion);
+
+			Usuario usuarioModificado = _usuarioLogic.FindUsuario(usuario.Correo);
+			Assert.AreEqual("Perez", usuarioModificado.Apellido);
+		}
 	}
 }
