d6ca9052921261e84bc7a0f8dcee3add4dd1e60f
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-05 14:01:07 -0300

    GREEN: ControladorRegistro_Registra_Nuevo_Usuario_UsuarioLogic

--- FinTrac/Controladores/ControladorRegistro.cs
+++ FinTrac/Controladores/ControladorRegistro.cs
@@ -12,5 +12,9 @@ namespace Controlador
 			_usuarioLogic = usuarioLogic;
 		}
 
+		public void RegistrarUsuario(string correo, string nombre, string apellido, string contrasena, string direccion)
+		{
+			_usuarioLogic.CrearUsuario(correo, nombre, apellido, contrasena, direccion);
+		}
 	}
 }
