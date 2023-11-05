aff5a013608f4865080c04eb100f5372a71340aa
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-05 14:56:14 -0300

    RED:  ControladorRegistro_Tiene_EspacioLogic

--- FinTrac/ControladorTest/ControladorRegistroTest.cs
+++ FinTrac/ControladorTest/ControladorRegistroTest.cs
@@ -9,17 +9,22 @@ namespace ControladorTest
 	[TestClass]
 	public class ControladorRegistroTest
 	{
-		private IRepository<Usuario> _repository;
+		private IRepository<Usuario> _repositorioUsuario;
 		private UsuarioLogic _usuarioLogic;
 		private UsuariosDbContext _context;
 		private readonly IDbContextFactory _contextFactory = new InMemoryDbContextFactory();
+		private IRepository<Espacio> _repositorioEspacio;
+		private EspacioLogic _espacioLogic;
 
 		[TestInitialize]
 		public void TestInitialize()
 		{
 			_context = _contextFactory.CreateDbContext();
-			_repository = new UsuarioMemoryRepository(_context);
-			_usuarioLogic = new UsuarioLogic(_repository);
+			_repositorioUsuario = new UsuarioMemoryRepository(_context);
+			_usuarioLogic = new UsuarioLogic(_repositorioUsuario);
+			_repositorioEspacio = new EspacioMemoryRepository(_context);
+			_espacioLogic = new EspacioLogic(_repositorioEspacio);
+
 			var usuario1 = new Usuario()
 			{
 				Correo = "hola@gmail.com",
@@ -46,25 +51,29 @@ namespace ControladorTest
 		{
 			_context.Database.EnsureDeleted();
 			_context.Dispose();
-			_context = null;
-			_repository = null;
-			_usuarioLogic = null;
 		}
 
 		[TestMethod]
 		public void ControladorRegistro_Tiene_UsuarioLogic()
 		{
-			ControladorRegistro controladorTest = new ControladorRegistro(_usuarioLogic);
+			ControladorRegistro controladorTest = new ControladorRegistro(_usuarioLogic, _espacioLogic);
 			Assert.IsNotNull(controladorTest);
 		}
 
 		[TestMethod]
 		public void ControladorRegistro_Registra_Nuevo_Usuario_UsuarioLogic()
 		{
-			ControladorRegistro controladorTest = new ControladorRegistro(_usuarioLogic);
+			ControladorRegistro controladorTest = new ControladorRegistro(_usuarioLogic, _espacioLogic);
 			controladorTest.RegistrarUsuario("test@gmail.com", "Alberto", "Lopez", "HOLAhola123", "Direccion 123");
 			Usuario usuarioRegistrado = _usuarioLogic.FindUsuario("test@gmail.com");
 			Assert.IsNotNull(usuarioRegistrado);
+		}
+
+		[TestMethod]
+		public void ControladorRegistro_Tiene_EspacioLogic()
+		{
+			ControladorRegistro controladorTest = new ControladorRegistro(_usuarioLogic,_espacioLogic);
+			Assert.IsNotNull(controladorTest);
 		}
 	}
 }
