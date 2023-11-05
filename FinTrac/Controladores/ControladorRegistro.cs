a63474760494f0bf353f261e3df39c8a9a01d06c
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-05 14:57:07 -0300

    GREEN:  ControladorRegistro_Tiene_EspacioLogic

--- FinTrac/Controladores/ControladorRegistro.cs
+++ FinTrac/Controladores/ControladorRegistro.cs
@@ -6,10 +6,12 @@ namespace Controlador
 	public class ControladorRegistro
 	{
 		private UsuarioLogic _usuarioLogic;
+		private EspacioLogic _espacioLogic;
 
-		public ControladorRegistro(UsuarioLogic usuarioLogic)
+		public ControladorRegistro(UsuarioLogic usuarioLogic, EspacioLogic espacioLogic)
 		{
 			_usuarioLogic = usuarioLogic;
+			_espacioLogic = espacioLogic;
 		}
 
 		public void RegistrarUsuario(string correo, string nombre, string apellido, string contrasena, string direccion)
