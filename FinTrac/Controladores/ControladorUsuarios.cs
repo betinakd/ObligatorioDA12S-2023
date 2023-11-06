6b00e3fc5d54553f3276f54f586cafdf64f28459
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-06 13:20:33 -0300

    GREEN: ControladorUsuarios_Tiene_UsuarioLogic

---
+++ FinTrac/Controladores/ControladorUsuarios.cs
@@ -0,0 +1,16 @@
+﻿using BussinesLogic;
+using Domain;
+
+
+namespace Controlador
+{
+	public class ControladorUsuarios
+	{
+		private UsuarioLogic _usuarioLogic;
+
+		public ControladorUsuarios(UsuarioLogic usuarioLogic)
+		{
+			_usuarioLogic = usuarioLogic;
+		}
+	}
+}
