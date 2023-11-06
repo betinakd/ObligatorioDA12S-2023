ea3ce93823968798b2e442c8cdc3ed754ad15f63
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-06 13:51:58 -0300

    RED:  ControladorUsuarios_DatosUsuariosInvitadosEspacio

--- FinTrac/ControladorTest/ControladorUsuariosTest.cs
+++ FinTrac/ControladorTest/ControladorUsuariosTest.cs
@@ -73,5 +73,43 @@ namespace ControladorTest
 			Assert.AreEqual("Perez", datos[1]);
 			Assert.AreEqual("hola@gmail.com", datos[2]);
 		}
+
+		[TestMethod]
+		public void ControladorUsuarios_DatosUsuariosInvitadosEspacio()
+		{
+			Usuario invitado1 = new Usuario()
+			{
+				Correo = "hola@gmail.com",
+				Nombre = "Juan",
+				Apellido = "Perez",
+				Contrasena = "123456789Aaa",
+				Direccion = "street 56 av rety"
+			};
+			Usuario invitado2 = new Usuario()
+			{
+				Correo = "chau@gmail.com",
+				Nombre = "Juana",
+				Apellido = "Perez",
+				Contrasena = "123456789Aawwa",
+				Direccion = "av bvar artigas 56"
+			};
+			Espacio espacio = new Espacio()
+			{
+				Id = 1,
+				Nombre = "Principal " + invitado1.Nombre,
+				Admin = invitado1
+			};
+			espacio.InvitarUsuario(invitado2);
+			_usuarioLogic.AddUsuario(invitado1);
+			_usuarioLogic.AddUsuario(invitado2);
+			_espacioLogic.AddEspacio(espacio);
+			ControladorUsuarios controladorTest = new ControladorUsuarios(_usuarioLogic, _espacioLogic);
+			string[,] resultado = controladorTest.DatosUsuariosInvitadosEspacio(1);
+			Assert.AreEqual("Juana", resultado[0, 0]);
+			Assert.AreEqual("Perez", resultado[0, 1]);
+			Assert.AreEqual("chau@gmail.com", resultado[0, 2]);
+			Assert.AreEqual(1, resultado.GetLength(0));
+			Assert.AreEqual(3, resultado.GetLength(1));
+		}
 	}
 }
