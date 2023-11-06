d1ed22857598a4b45848b871deed76a7c6fb6178
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-06 13:20:25 -0300

    RED: ControladorUsuarios_Tiene_UsuarioLogic

---
+++ FinTrac/ControladorTest/ControladorUsuariosTest.cs
@@ -0,0 +1,62 @@
+﻿using BussinesLogic;
+using Controlador;
+using Domain;
+using Repository;
+
+namespace ControladorTest
+{
+	[TestClass]
+	public class ControladorUsuariosTest
+	{
+		private IRepository<Usuario> _repositorioUsuario;
+		private UsuarioLogic _usuarioLogic;
+		private UsuariosDbContext _context;
+		private readonly IDbContextFactory _contextFactory = new InMemoryDbContextFactory();
+		private IRepository<Espacio> _repositorioEspacio;
+		private EspacioLogic _espacioLogic;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_context = _contextFactory.CreateDbContext();
+			_repositorioUsuario = new UsuarioMemoryRepository(_context);
+			_usuarioLogic = new UsuarioLogic(_repositorioUsuario);
+			_repositorioEspacio = new EspacioMemoryRepository(_context);
+			_espacioLogic = new EspacioLogic(_repositorioEspacio);
+
+			var usuario1 = new Usuario()
+			{
+				Correo = "hola@gmail.com",
+				Nombre = "Juan",
+				Apellido = "Perez",
+				Contrasena = "123456789Aaa",
+				Direccion = "street 56 av rety"
+			};
+
+			var usuario2 = new Usuario()
+			{
+				Correo = "holaSoy2@gmail.com",
+				Nombre = "Alberto",
+				Apellido = "Rodriguez",
+				Contrasena = "123tttt9Aaa",
+				Direccion = "street 67 av white"
+			};
+			_usuarioLogic.AddUsuario(usuario1);
+			_usuarioLogic.AddUsuario(usuario2);
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			_context.Database.EnsureDeleted();
+			_context.Dispose();
+		}
+
+		[TestMethod]
+		public void ControladorUsuarios_Tiene_UsuarioLogic()
+		{
+			ControladorUsuarios controladorTest = new ControladorUsuarios(_usuarioLogic);
+			Assert.IsNotNull(controladorTest);
+		}
+	}
+}
