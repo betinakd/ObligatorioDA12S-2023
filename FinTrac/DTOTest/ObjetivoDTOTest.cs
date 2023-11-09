0cf288d026d25d239e17b8fdfb5281b15b75dc6d
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 15:32:17 -0300

    RED: ObjetivoDTO_Tiene_Categorias

--- FinTrac/DTOTest/ObjetivoDTOTest.cs
+++ FinTrac/DTOTest/ObjetivoDTOTest.cs
@@ -1,5 +1,5 @@
 ﻿using DTO;
-
+using DTO.EnumsDTO;
 namespace DTOTest
 {
 	[TestClass]
@@ -21,6 +21,24 @@ namespace DTOTest
 			double montoMaximo = 1000;
 			objetivoDTO.MontoMaximo = montoMaximo;
 			Assert.AreEqual(montoMaximo, objetivoDTO.MontoMaximo);
+		}
+
+		[TestMethod]
+		public void ObjetivoDTO_Tiene_Categorias()
+		{
+			ObjetivoDTO objetivoDTO = new ObjetivoDTO();
+			List<CategoriaDTO> categorias = new List<CategoriaDTO>();
+			var categoria = new CategoriaDTO()
+			{
+				Nombre = "Categoria 1",
+				EstadoActivo = true,
+				FechaCreacion = DateTime.Now,
+				Tipo = TipoCategoriaDTO.Costo
+			};
+			categorias.Add(categoria);
+			objetivoDTO.Categorias = categorias;
+			Assert.AreEqual(categorias, objetivoDTO.Categorias);
+			Assert.AreEqual(1, objetivoDTO.Categorias.Count);
 		}
 	}
 }
