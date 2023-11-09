8cd29e1e954ff980f3bbec85c364d9dddd99a3f8
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 17:21:41 -0300

    GREEN: EspacioDTO_Tiene_Transacciones

--- FinTrac/DTO/EspacioDTO.cs
+++ FinTrac/DTO/EspacioDTO.cs
@@ -5,10 +5,11 @@
 		public int Id { get; set; }	
 		public string Nombre { get; set; }
 		public UsuarioDTO Admin { get; set; }
-		public List<UsuarioDTO> UsuariosInvitados { get; set; }
+		public List<UsuarioDTO> UsuariosInvitados { get; set; } = 
 		public List<CategoriaDTO> Categorias { get; set; }
 		public List<CambioDTO> Cambios { get; set; }
 		public List<ObjetivoDTO> Objetivos { get; set; }
 		public List<CuentaDTO> Cuentas { get; set; }
+		public List<TransaccionDTO> Transacciones { get; set; }
 	}
 }
