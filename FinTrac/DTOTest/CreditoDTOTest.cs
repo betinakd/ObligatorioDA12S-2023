8ad59746ed4728cf49d3bf8d7237856e05fc93e0
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 16:01:46 -0300

    RED: CreditoDTO_Tiene_NumeroTarjeta

--- FinTrac/DTOTest/CreditoDTOTest.cs
+++ FinTrac/DTOTest/CreditoDTOTest.cs
@@ -12,5 +12,13 @@ namespace DTOTest
 			creditoDTO.BancoEmisor = "Banco de Chile";
 			Assert.AreEqual("Banco de Chile", creditoDTO.BancoEmisor);
 		}
+
+		[TestMethod]
+		public void CreditoDTO_Tiene_NumeroTarjeta()
+		{
+			var creditoDTO = new CreditoDTO();
+			creditoDTO.NumeroTarjeta = "1234";
+			Assert.AreEqual("1234", creditoDTO.NumeroTarjeta);
+		}
 	}
 }
