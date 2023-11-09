45200166c2a47a5c8ef9c6585cd5a336541c5b46
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 15:40:08 -0300

    RED: CuentaDTO_Tiene_Moneda

---
+++ FinTrac/DTOTest/CuentaDTOTest.cs
@@ -0,0 +1,23 @@
+﻿using DTO.EnumsDTO;
+using DTO;
+
+namespace DTOTest
+{
+	[TestClass]
+	public class CuentaDTOTest
+	{
+		[TestMethod]
+		public void CuentaDTO_Tiene_Moneda()
+		{
+			CuentaDTO cuentaDTO = new CuentaDTO();
+			cuentaDTO.Moneda = TipoCambiarioDTO.PesosUruguayos;
+			CuentaDTO cuentaDTO2 = new CuentaDTO();
+			cuentaDTO.Moneda = TipoCambiarioDTO.Euro;
+			CuentaDTO cuentaDTO3 = new CuentaDTO();
+			cuentaDTO.Moneda = TipoCambiarioDTO.Dolar;
+			Assert.AreEqual(cuentaDTO.Moneda, TipoCambiarioDTO.Dolar);
+			Assert.AreEqual(cuentaDTO.Moneda, TipoCambiarioDTO.Euro);
+			Assert.AreEqual(cuentaDTO.Moneda, TipoCambiarioDTO.PesosUruguayos);
+		}
+	}
+}
