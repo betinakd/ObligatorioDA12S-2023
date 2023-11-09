a1ef0a5d3520ebf2438804020e0ee7d254b27b95
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 15:58:46 -0300

    RED: CreditoDTO_Tiene_BancoEmisor

--- FinTrac/DTO/AhorroDTO.cs
+++ FinTrac/DTO/AhorroDTO.cs
@@ -1,7 +1,7 @@
 ﻿using DTO.EnumsDTO;
 namespace DTO
 {
-	public class AhorroDTO
+	public class AhorroDTO : CuentaDTO
 	{
 		public double Monto { get; set; }
 		public string Nombre { get; set; }
---
+++ FinTrac/DTOTest/CreditoDTOTest.cs
@@ -0,0 +1,16 @@
+﻿using DTO;
+
+namespace DTOTest
+{
+	[TestClass]
+	public class CreditoDTOTest
+	{
+		[TestMethod]
+		public void CreditoDTO_Tiene_BancoEmisor()
+		{
+			var creditoDTO = new CreditoDTO();
+			creditoDTO.BancoEmisor = "Banco de Chile";
+			Assert.AreEqual("Banco de Chile", creditoDTO.BancoEmisor);
+		}
+	}
+}
