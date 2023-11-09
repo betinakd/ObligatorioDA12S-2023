9d4cc449e3e387e8c27299829933be6ef6996e82
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 15:20:45 -0300

    GREEN: CambioDTO_Tiene_Moneda

---
+++ FinTrac/DTOTest/CambioDTOTest.cs
@@ -0,0 +1,23 @@
+﻿using DTO;
+using DTO.EnumsDTO;
+
+namespace DTOTest
+{
+	[TestClass]
+	public class CambioDTOTest
+	{
+		[TestMethod]
+		public void CambioDTO_Tiene_Moneda()
+		{ 
+			CambioDTO cambio = new CambioDTO();
+			cambio.Moneda = TipoCambiarioDTO.Dolar;
+			CambioDTO cambio2 = new CambioDTO();
+			cambio2.Moneda = TipoCambiarioDTO.Euro;
+			CambioDTO cambio3 = new CambioDTO();
+			cambio3.Moneda = TipoCambiarioDTO.PesosUruguayos;
+			Assert.AreEqual(TipoCambiarioDTO.PesosUruguayos, cambio3.Moneda);
+			Assert.AreEqual(TipoCambiarioDTO.Euro, cambio2.Moneda);
+			Assert.AreEqual(TipoCambiarioDTO.Dolar, cambio.Moneda);
+		}
+	}
+}
