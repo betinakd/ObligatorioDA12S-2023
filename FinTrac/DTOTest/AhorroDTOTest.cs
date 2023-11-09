c909694979df0389e403b1fff3179b6d32a6e211
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 15:54:05 -0300

    RED: AhorroDTO_Tiene_Nombre

--- FinTrac/DTOTest/AhorroDTOTest.cs
+++ FinTrac/DTOTest/AhorroDTOTest.cs
@@ -6,11 +6,19 @@ namespace DTOTest
 	public class AhorroDTOTest
 	{
 		[TestMethod]
-		public void MontoTest()
+		public void AhorroDTO_Tiene_Monto()
 		{
 			AhorroDTO ahorroDTO = new AhorroDTO();
 			ahorroDTO.Monto = 1000;
 			Assert.AreEqual(1000, ahorroDTO.Monto);
+		}
+
+		[TestMethod]
+		public void AhorroDTO_Tiene_Nombre()
+		{
+			AhorroDTO ahorroDTO = new AhorroDTO();
+			ahorroDTO.Nombre = "Ahorro";
+			Assert.AreEqual("Ahorro", ahorroDTO.Nombre);
 		}
 
 	}
