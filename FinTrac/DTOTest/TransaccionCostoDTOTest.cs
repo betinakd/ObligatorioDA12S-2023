67c5eeadb0d9df63d7c21a4cc5f47900d64264f4
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 16:55:21 -0300

    GREEN: TransaccionCosto_No_Es_Null

--- FinTrac/DTOTest/TransaccionCostoDTOTest.cs
+++ FinTrac/DTOTest/TransaccionCostoDTOTest.cs
@@ -3,7 +3,14 @@ using DTO.EnumsDTO;
 
 namespace DTOTest
 {
+	[TestClass]
 	public class TransaccionCostoDTOTest
 	{
+		[TestMethod]
+		public void TransaccionCosto_No_Es_Null()
+		{
+			TransaccionDTO transaccionDTO = new TransaccionCostoDTO();
+			Assert.IsNotNull(transaccionDTO);
+		}
 	}
 }
