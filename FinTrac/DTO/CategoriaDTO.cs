be02b6534b23a61a85469872cd5da8d508e8fde4
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 17:24:54 -0300

    GREEN: CategoriaDTO_Tiene_Id

--- FinTrac/DTO/CategoriaDTO.cs
+++ FinTrac/DTO/CategoriaDTO.cs
@@ -4,6 +4,7 @@ namespace DTO
 {
 	public class CategoriaDTO
 	{
+		public int Id { get; set; }
 		public string Nombre { get; set; }
 		public bool EstadoActivo { get; set; }
 		public DateTime FechaCreacion { get; set; }
