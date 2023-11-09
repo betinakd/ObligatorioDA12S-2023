9a689141dc185cc651ca778e66913dd055aebe08
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 14:58:04 -0300

    RED: EspacioDTO_Tiene_Nombre

---
+++ FinTrac/DTOTest/EspacioDTOTest.cs
@@ -0,0 +1,16 @@
+﻿using DTO;
+namespace DTOTest
+{
+	[TestClass]
+	public class EspacioDTOTest
+	{
+		[TestMethod]
+		public void EspacioDTO_Tiene_Nombre()
+		{
+			EspacioDTO espacio = new EspacioDTO();
+			string nombre = "EspacioTest";
+			espacio.Nombre = nombre;
+			Assert.AreEqual(nombre, espacio.Nombre);
+		}
+	}
+}
