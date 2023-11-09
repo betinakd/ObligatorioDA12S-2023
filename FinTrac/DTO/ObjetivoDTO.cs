61ec6f6863016228a1495832cd7e7573672ce2d1
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 17:26:07 -0300

    GREEN: ObjetivoDTO_Tiene_Id

--- FinTrac/DTO/ObjetivoDTO.cs
+++ FinTrac/DTO/ObjetivoDTO.cs
@@ -2,6 +2,7 @@
 {
 	public class ObjetivoDTO
 	{ 
+		public int Id { get; set; }
 		public string Titulo { get; set; }
 		public double MontoMaximo { get; set; }
 		public List<CategoriaDTO> Categorias { get; set; }
