14c934693dc7683de83e12e631cbd6a9c38b7cd8
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 15:27:48 -0300

    RED: ObjetivoDTO_Tiene_Titulo

---
+++ FinTrac/DTOTest/ObjetivoDTOTest.cs
@@ -0,0 +1,17 @@
+﻿using DTO;
+
+namespace DTOTest
+{
+	[TestClass]
+	public class ObjetivoDTOTest
+	{
+		[TestMethod]
+		public void ObjetivoDTO_Tiene_Titulo()
+		{
+			ObjetivoDTO objetivoDTO = new ObjetivoDTO();
+			string titulo = "Objetivo 1";
+			objetivoDTO.Titulo = titulo;
+			Assert.AreEqual(titulo, objetivoDTO.Titulo);
+		}
+	}
+}
