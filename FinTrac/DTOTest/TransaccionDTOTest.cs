4abd339b5daf4ca2bdf4633621b7907332bd2cfb
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-09 16:12:44 -0300

    RED: TransaccionDTO_Tiene_Id

---
+++ FinTrac/DTOTest/TransaccionDTOTest.cs
@@ -0,0 +1,17 @@
+﻿using DTO;
+using DTO.EnumsDTO;
+
+namespace DTOTest
+{
+	[TestClass]
+	public class TransaccionDTOTest
+	{
+		[TestMethod]
+		public void TransaccionDTO_Tiene_Id()
+		{
+			TransaccionDTO transaccionDTO = new TransaccionDTO();
+			transaccionDTO.Id = 1;
+			Assert.AreEqual(1, transaccionDTO.Id);
+		}
+	}
+}
