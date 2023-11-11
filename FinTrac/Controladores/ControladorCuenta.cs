7b38dede5860af2f58c80a0f6dadad8addb2ae96
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-10 21:36:20 -0300

    GREEN: ControladorCuenta_CreditosDeEspacio_Retorna_ListaDeCreditos

--- FinTrac/Controladores/ControladorCuenta.cs
+++ FinTrac/Controladores/ControladorCuenta.cs
@@ -32,6 +32,26 @@ namespace Controlador
 				.ToList();
 			return ahorros;
 		}
+
+		public List<CreditoDTO> CreditosDeEspacio(int espacioId)
+		{
+			Espacio espacio = _espacioLogic.FindEspacio(espacioId);
+			List<CreditoDTO> creditos = new List<CreditoDTO>();
+
+			creditos = espacio.Cuentas
+				.OfType<Credito>()
+				.Select(credito => new CreditoDTO
+				{
+					Id = credito.Id,
+					BancoEmisor = credito.BancoEmisor,
+					NumeroTarjeta = credito.NumeroTarjeta,
+					FechaCreacion = credito.FechaCreacion,
+					FechaCierre = credito.FechaCierre,
+					CreditoDisponible = credito.CreditoDisponible,
+				})
+				.ToList();
+			return creditos;
+		}
 	}
 
 
