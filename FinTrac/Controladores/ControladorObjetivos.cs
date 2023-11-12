1d01428c65ce1fdb5f3b61652444a022ed1c8131
maxxi98 <maxi_nos_10@hotmail.com>
2023-11-12 17:08:19 -0300

    GREEN: ControladorObjetivos_ObjetivosDeEspacio_TieneObjetivo

--- FinTrac/Controladores/ControladorObjetivos.cs
+++ FinTrac/Controladores/ControladorObjetivos.cs
@@ -20,7 +20,42 @@ namespace Controlador
 			Espacio espacio = _objetivoLogic.FindEspacio(id);
 			List<Objetivo> objetivos = espacio.Objetivos;
 			List<ObjetivoDTO> objetivosDTO = new List<ObjetivoDTO>();
+			foreach (Objetivo objetivo in objetivos)
+			{
+				ObjetivoDTO objetivoDTO = new ObjetivoDTO()
+				{
+					Id = objetivo.Id,
+					Titulo = objetivo.Titulo,
+					MontoMaximo = objetivo.MontoMaximo,
+					Categorias = Cambiar_CategoriasDTO(id, objetivo.Categorias),
+					Token = objetivo.Token
+				};
+				objetivosDTO.Add(objetivoDTO);
+			}
 			return objetivosDTO;
+		}
+
+		private List<CategoriaDTO> Cambiar_CategoriasDTO(int id, List<Categoria> categorias)
+		{
+			List<CategoriaDTO> categoriasDTO = new List<CategoriaDTO>();
+			foreach (Categoria categoria in categorias)
+			{
+				CategoriaDTO categoriaDTO = new CategoriaDTO()
+				{
+					Id = categoria.Id,
+					Nombre = categoria.Nombre,
+					EstadoActivo = categoria.EstadoActivo,
+					FechaCreacion = categoria.FechaCreacion,
+					Tipo = Cambiar_TipoCategoria(categoria.Tipo)
+				};
+				categoriasDTO.Add(categoriaDTO);
+			}
+			return categoriasDTO;
+		}
+
+		private TipoCategoriaDTO Cambiar_TipoCategoria(TipoCategoria tipoCategoria)
+		{
+			return TipoCategoriaDTO.Costo;
 		}
 	}
 }
