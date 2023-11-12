e3cf9f1806d022be3760a1f2e59cb8e70740d6f0
maxxi98 <maxi_nos_10@hotmail.com>
2023-11-12 16:56:26 -0300

    RED: ControladorObjetivos_Tiene_EspacioLogic

---
+++ FinTrac/ControladorTest/ControladorObjetivosTest.cs
@@ -0,0 +1,64 @@
+﻿using BussinesLogic;
+using Domain;
+using Repository;
+using Controlador;
+using DTO;
+using DTO.EnumsDTO;
+
+namespace ControladorTest
+{
+	[TestClass]
+	public class ControladorObjetivosTest
+	{
+		private IRepository<Usuario> _repositorioUsuario;
+		private UsuarioLogic _usuarioLogic;
+		private UsuariosDbContext _context;
+		private readonly IDbContextFactory _contextFactory = new InMemoryDbContextFactory();
+		private IRepository<Espacio> _repositorioEspacio;
+		private EspacioLogic _espacioLogic;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_context = _contextFactory.CreateDbContext();
+			_repositorioUsuario = new UsuarioMemoryRepository(_context);
+			_usuarioLogic = new UsuarioLogic(_repositorioUsuario);
+			_repositorioEspacio = new EspacioMemoryRepository(_context);
+			_espacioLogic = new EspacioLogic(_repositorioEspacio);
+
+			var usuario1 = new Usuario()
+			{
+				Correo = "Juan@a.com",
+				Nombre = "Juan",
+				Apellido = "Perez",
+				Contrasena = "123456789Aaa",
+				Direccion = "street 56 av rety"
+			};
+
+			var usuario2 = new Usuario()
+			{
+				Correo = "Alberto@a.com",
+				Nombre = "Alberto",
+				Apellido = "Rodriguez",
+				Contrasena = "123tttt9Aaa",
+				Direccion = "street 67 av white"
+			};
+			_usuarioLogic.AddUsuario(usuario1);
+			_usuarioLogic.AddUsuario(usuario2);
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			_context.Database.EnsureDeleted();
+			_context.Dispose();
+		}
+
+		[TestMethod]
+		public void ControladorObjetivos_Tiene_EspacioLogic()
+		{
+			ControladorObjetivos controladorTest = new ControladorObjetivos(_espacioLogic);
+			Assert.IsNotNull(controladorTest);
+		}
+	}
+}
