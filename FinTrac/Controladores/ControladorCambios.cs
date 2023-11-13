9c2cf9db978dd9a013e95bb0c09255385a18ad93
maxxi98 <maxi_nos_10@hotmail.com>
2023-11-13 15:12:37 -0300

    GREEN: ControladorCambios_CambiosDeEspacio

--- FinTrac/Controladores/ControladorCambios.cs
+++ FinTrac/Controladores/ControladorCambios.cs
@@ -14,5 +14,12 @@ namespace Controlador
 		{
 			_cambioLogic = cambioLogic;
 		}
+
+		public List<CambioDTO> CambiosDeEspacio(int id)
+		{
+			Espacio espacio = _cambioLogic.FindEspacio(id);
+			List<CambioDTO> cambiosDTO = new List<CambioDTO>();
+			return cambiosDTO;
+		}
 	}
 }
