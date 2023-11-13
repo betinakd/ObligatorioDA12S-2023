1abaf144ae16d1c4615645bfa86e1c9f09ca5b68
maxxi98 <maxi_nos_10@hotmail.com>
2023-11-13 15:30:32 -0300

    GREEN: ControladorCambios_CambiosDeEspacio_TieneCambio

--- FinTrac/Controladores/ControladorCambios.cs
+++ FinTrac/Controladores/ControladorCambios.cs
@@ -18,8 +18,36 @@ namespace Controlador
 		public List<CambioDTO> CambiosDeEspacio(int id)
 		{
 			Espacio espacio = _cambioLogic.FindEspacio(id);
+			List<Cambio> cambios = espacio.Cambios;
 			List<CambioDTO> cambiosDTO = new List<CambioDTO>();
+			foreach (Cambio cambio in cambios)
+			{
+				CambioDTO cambioDTO = new CambioDTO()
+				{
+					Id = cambio.Id,
+					Moneda = Cambiar_TipoCambiarioDTO(cambio.Moneda),
+					FechaDeCambio = cambio.FechaDeCambio,
+					Pesos = cambio.Pesos
+				};
+				cambiosDTO.Add(cambioDTO);
+			}
 			return cambiosDTO;
+		}
+
+		private TipoCambiarioDTO Cambiar_TipoCambiarioDTO(TipoCambiario tipo)
+		{
+			TipoCambiarioDTO tipoResulatado = TipoCambiarioDTO.Dolar;
+			if (tipo != TipoCambiario.PesosUruguayos)
+			{
+				foreach (TipoCambiarioDTO tipoDTO in Enum.GetValues(typeof(TipoCambiarioDTO)))
+				{
+					if (tipoDTO.ToString() == tipo.ToString())
+					{
+						tipoResulatado = tipoDTO;
+					}
+				}
+			}
+			return tipoResulatado;
 		}
 	}
 }
