a5c1f8a07be41f071d707ec5ddd297790f4b0e74
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-14 05:15:57 -0300

    GREEN: ControladorSesion_Valida_Usuario_Por_Correo_Contrasena

---
+++ FinTrac/Controladores/ControladorSesion.cs
@@ -0,0 +1,31 @@
+﻿using BussinesLogic;
+using DTO;
+using Excepcion;
+using Domain;
+
+namespace Controlador
+{
+	public class ControladorSesion
+	{
+		EspacioLogic _espacioLogic;
+		UsuarioLogic _usuarioLogic;
+
+
+		public ControladorSesion(UsuarioLogic usuarioLogic, EspacioLogic espacioLogic)
+		{
+			_usuarioLogic = usuarioLogic;
+			_espacioLogic = espacioLogic;
+		}
+
+		public string ValidarInicioSesion(string correo, string contrasena)
+		{
+			string errorMsj = "";
+
+				_usuarioLogic.UsuarioByCorreoContrasena(correo, contrasena);
+
+			return errorMsj;
+		}
+
+
+	}
+}
