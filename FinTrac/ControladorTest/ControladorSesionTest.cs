aefc0b820d9525692c99a09dc9b19bad2d98528c
Ana Betina Kadessian <ak221509@fi365.ort.edu.uy>
2023-11-14 05:15:15 -0300

    RED: ControladorSesion_Valida_Usuario_Por_Correo_Contrasena

---
+++ FinTrac/ControladorTest/ControladorSesionTest.cs
@@ -0,0 +1,79 @@
+﻿using BussinesLogic;
+using Domain;
+using Repository;
+using Controlador;
+
+namespace ControladorTest
+{
+	[TestClass]
+	public class ControladorSesionTest
+	{
+		private IRepository<Usuario> _repositorioUsuario;
+		private UsuarioLogic _usuarioLogic;
+		private UsuariosDbContext _context;
+		private readonly IDbContextFactory _contextFactory = new InMemoryDbContextFactory();
+		private IRepository<Espacio> _repositorioEspacio;
+		private EspacioLogic _espacioLogic;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_context = _contextFactory.CreateDbContext();
+			_repositorioUsuario = new UsuarioMemoryRepository(_context);
+			_usuarioLogic = new UsuarioLogic(_repositorioUsuario);
+			_repositorioEspacio = new EspacioMemoryRepository(_context);
+			_espacioLogic = new EspacioLogic(_repositorioEspacio);
+
+			var usuario1 = new Usuario()
+			{
+				Correo = "hola@gmail.com",
+				Nombre = "Juan",
+				Apellido = "Perez",
+				Contrasena = "123456789Aaa",
+				Direccion = "street 56 av rety"
+			};
+
+			var usuario2 = new Usuario()
+			{
+				Correo = "holaSoy2@gmail.com",
+				Nombre = "Alberto",
+				Apellido = "Rodriguez",
+				Contrasena = "123tttt9Aaa",
+				Direccion = "street 67 av white"
+			};
+			_usuarioLogic.AddUsuario(usuario1);
+			_usuarioLogic.AddUsuario(usuario2);
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			_context.Database.EnsureDeleted();
+			_context.Dispose();
+		}
+
+		[TestMethod]
+		public void ControladorSesion_Valida_Usuario_Por_Correo_Contrasena()
+		{
+			Usuario usuario = new Usuario
+			{
+				Nombre = "Usuario",
+				Apellido = "Test",
+				Correo = "test@gmail.com",
+				Contrasena = "TestTest12",
+				Direccion = "Av test"
+			};
+			_usuarioLogic.AddUsuario(usuario);
+			Espacio espacio = new Espacio
+			{
+				Nombre = "Espacio",
+				Id = 1,
+				Admin = usuario
+			};
+			_espacioLogic.AddEspacio(espacio);
+			ControladorSesion controladorTest = new ControladorSesion(_usuarioLogic, _espacioLogic);
+			string mensaje = controladorTest.ValidarInicioSesion(usuario.Correo, usuario.Contrasena);
+			Assert.AreEqual("", mensaje);
+		}
+	}
+}
