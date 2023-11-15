849257056117c867f84f8b3d314d7d4e4f5cc0ee
Mateo Arias <mateoarias437@gmail.com>
2023-11-14 23:31:37 -0300

    RED: IngresoEgresoDTO_Tiene_Fecha

---
+++ FinTrac/DTOTest/IngresoEgresoDTOTest.cs
@@ -0,0 +1,16 @@
+﻿using DTO;
+using DTO.EnumsDTO;
+namespace DTOTest
+{
+	[TestClass]
+	public class IngresoEgresoDTOTest
+	{
+		[TestMethod]
+		public void IngresoEgresoDTO_Tiene_Fecha()
+		{
+			IngresoEgresoDTO ingresoEgreso = new IngresoEgresoDTO();
+			ingresoEgreso.Fecha = DateTime.Today;
+			Assert.AreNotEqual(DateTime.Today, ingresoEgreso.Fecha);
+		}
+	}
+}
