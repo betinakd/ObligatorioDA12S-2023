04c0e9516a5ef93b1cb4b2b8f76cd0816938d06a
Mateo Arias <mateoarias437@gmail.com>
2023-11-15 01:16:38 -0300

    RED: ReporteListadoGastos_Crea_Elemento_Cuenta_Ahorro_Pesos

--- FinTrac/Controladores/ControladorReporte.cs
+++ FinTrac/Controladores/ControladorReporte.cs
@@ -43,7 +43,95 @@ namespace Controlador
 			return reporteEnDTO;
 		}
 
-		
+		public List<TransaccionDTO> ReporteDeGastos(int id, CategoriaDTO _catFiltro, DateTime _fechaIni, DateTime _fechaFin, CuentaDTO _cuentaFiltro)
+		{
+			Espacio espacio = _reporte.FindEspacio(id);
+			Reporte reporte = new Reporte(espacio);
+			Categoria categoria = CategoriaDTO_A_Categoria(_catFiltro);
+			Cuenta cuenta;
+			if (_cuentaFiltro is CreditoDTO)
+			{
+				cuenta = CreditoDTO_A_Credito((CreditoDTO)_cuentaFiltro);
+			}
+			else
+			{
+				cuenta = AhorroDTO_A_Ahorro((AhorroDTO)_cuentaFiltro);
+			}
+			List<Transaccion> reporteDeGastos = reporte.ListadoGastos(categoria, _fechaIni, _fechaFin, cuenta);
+			List<TransaccionDTO> transaccionDTOs = new List<TransaccionDTO>();
+			foreach (Transaccion transaccion in reporteDeGastos)
+			{
+				TransaccionDTO transaccionDTO;
+				Cuenta CuentaTransaccion;
+				if (transaccion.CuentaMonetaria is Credito)
+				{
+					CuentaTransaccion = (Credito)transaccion.CuentaMonetaria;
+				} else
+				{
+					CuentaTransaccion = (Ahorro)transaccion.CuentaMonetaria;
+				}
+				if (transaccion.Moneda.Equals(TipoCambiario.Dolar))
+				{
+					transaccionDTO = new TransaccionDTO()
+					{
+						CategoriaTransaccion = new CategoriaDTO()
+						{
+							EstadoActivo = transaccion.CategoriaTransaccion.EstadoActivo,
+							FechaCreacion = transaccion.CategoriaTransaccion.FechaCreacion,
+							Id = transaccion.CategoriaTransaccion.Id,
+							Nombre = transaccion.CategoriaTransaccion.Nombre,
+							Tipo = TipoCategoriaDTO.Costo,
+						},
+						CuentaMonetaria = _cuentaFiltro,
+						FechaTransaccion = transaccion.FechaTransaccion,
+						Id = transaccion.Id,
+						Moneda = TipoCambiarioDTO.Dolar,
+						Monto = transaccion.Monto,
+						Titulo = transaccion.Titulo,
+					};
+				} else if (transaccion.Moneda.Equals(TipoCambiario.Euro))
+				{
+					transaccionDTO = new TransaccionDTO()
+					{
+						CategoriaTransaccion = new CategoriaDTO()
+						{
+							EstadoActivo = transaccion.CategoriaTransaccion.EstadoActivo,
+							FechaCreacion = transaccion.CategoriaTransaccion.FechaCreacion,
+							Id = transaccion.CategoriaTransaccion.Id,
+							Nombre = transaccion.CategoriaTransaccion.Nombre,
+							Tipo = TipoCategoriaDTO.Costo,
+						},
+						CuentaMonetaria = _cuentaFiltro,
+						FechaTransaccion = transaccion.FechaTransaccion,
+						Id = transaccion.Id,
+						Moneda = TipoCambiarioDTO.Euro,
+						Monto = transaccion.Monto,
+						Titulo = transaccion.Titulo,
+					};
+				} else
+				{
+					transaccionDTO = new TransaccionDTO()
+					{
+						CategoriaTransaccion = new CategoriaDTO()
+						{
+							EstadoActivo = transaccion.CategoriaTransaccion.EstadoActivo,
+							FechaCreacion = transaccion.CategoriaTransaccion.FechaCreacion,
+							Id = transaccion.CategoriaTransaccion.Id,
+							Nombre = transaccion.CategoriaTransaccion.Nombre,
+							Tipo = TipoCategoriaDTO.Costo,
+						},
+						CuentaMonetaria = _cuentaFiltro,
+						FechaTransaccion = transaccion.FechaTransaccion,
+						Id = transaccion.Id,
+						Moneda = TipoCambiarioDTO.PesosUruguayos,
+						Monto = transaccion.Monto,
+						Titulo = transaccion.Titulo,
+					};
+				}
+				transaccionDTOs.Add(transaccionDTO);
+			}
+			return transaccionDTOs;
+		}
 
 
 		private Ahorro AhorroDTO_A_Ahorro(AhorroDTO account)
