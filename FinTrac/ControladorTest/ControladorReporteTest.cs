d6a3b6d8586300ef5b49ee47ce0c9cbef47b716f
Mateo Arias <mateoarias437@gmail.com>
2023-11-15 00:09:15 -0300

    RED: ControladorReporte_Se_Crea

---
+++ FinTrac/ControladorTest/ControladorReporteTest.cs
@@ -0,0 +1,70 @@
+﻿using Controlador;
+using DTO;
+using Domain;
+using EspacioReporte;
+using DTO.EnumsDTO;
+using BussinesLogic;
+using Repository;
+
+namespace ControladorTest
+{
+
+	
+
+	[TestClass]
+	public class ControladorReporteTest
+	{
+		private IRepository<Espacio> _repositorioEspacio;
+		private EspacioLogic _espacioLogic;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_espacioLogic = new EspacioLogic(_repositorioEspacio);
+		}
+
+		[TestMethod]
+		public void ControladorReporte_Se_Crea()
+		{
+			ControladorReporte controladorReporte = new ControladorReporte(_espacioLogic);
+			Assert.IsNull(controladorReporte);
+		}
+
+
+		[TestMethod]
+		public void ReporteObjetivosGasto_Existe()
+		{
+			Espacio espacio = new Espacio() { Id = 1 };
+			Categoria categoria = new Categoria()
+			{
+				EstadoActivo = true,
+				FechaCreacion = DateTime.Today,
+				Id = 1,
+				Nombre = "PruebaCat",
+				Tipo = TipoCategoria.Costo,
+			};
+			espacio.AgregarCategoria(categoria);
+			List<Categoria> listaCategorias = new List<Categoria>();
+			listaCategorias.Add(categoria);
+			Objetivo objetivo = new Objetivo()
+			{
+				Categorias = listaCategorias,
+				Id = 1,
+				MontoMaximo = 1000,
+				Titulo = "ObjetivoPrueba",
+				Token = "token",
+			};
+			espacio.AgregarObjetivo(objetivo);
+			Transaccion transaccion = new Transaccion()
+			{
+				Moneda = TipoCambiario.PesosUruguayos,
+				Monto = 100,
+				CategoriaTransaccion = categoria,
+			};
+			espacio.AgregarTransaccion(transaccion);
+			ControladorReporte controladorReporte = new ControladorReporte(_espacioLogic);
+			List<ObjetivoGastoDTO> reporteControlador = controladorReporte.ReporteObjetivosGastos(1);
+			Assert.IsTrue(reporteControlador.Count == 1);
+		}
+	}
+}
--- FinTrac/Controladores/ControladorReporte.cs
+++ FinTrac/Controladores/ControladorReporte.cs
@@ -1,1 +1,171 @@
-﻿
+﻿using BussinesLogic;
+using DTO;
+using DTO.EnumsDTO;
+using Domain;
+using Excepcion;
+using EspacioReporte;
+using System;
+
+namespace Controlador
+{
+	public class ControladorReporte
+	{
+		private EspacioLogic _reporte;
+
+		public ControladorReporte(EspacioLogic reporte)
+		{
+			_reporte = reporte;
+		}
+
+		public List<ObjetivoGastoDTO> ReporteObjetivosGastos(int id)
+		{
+			Espacio espacio = _reporte.FindEspacio(id);
+			Reporte reporte = new Reporte(espacio);
+			List<ObjetivoGasto> reporteObjetivoGasto = reporte.ReporteObjetivosDeGastos();
+			List<ObjetivoGastoDTO> reporteEnDTO = new List<ObjetivoGastoDTO>();
+			foreach (ObjetivoGasto objetivoGasto in reporteObjetivoGasto)
+			{
+				ObjetivoGastoDTO obj = new ObjetivoGastoDTO()
+				{
+					MontoAcumulado = objetivoGasto.MontoAcumulado,
+					MontoEsperado = objetivoGasto.MontoEsperado,
+					Objetivo = new ObjetivoDTO()
+					{
+						Categorias = CategoriasA_DTO(objetivoGasto.Objetivo.Categorias),
+						Id = objetivoGasto.Objetivo.Id,
+						MontoMaximo = objetivoGasto.Objetivo.MontoMaximo,
+						Titulo = objetivoGasto.Objetivo.Titulo,
+						Token = objetivoGasto.Objetivo.Token,
+					}
+				};
+				reporteEnDTO.Add(obj);
+			}
+			return reporteEnDTO;
+		}
+
+		
+
+
+		private Ahorro AhorroDTO_A_Ahorro(AhorroDTO account)
+		{
+			Ahorro cuenta;
+			if (account.Moneda.Equals(TipoCambiarioDTO.Euro))
+			{
+				cuenta = new Ahorro()
+				{
+					FechaCreacion = account.FechaCreacion,
+					Moneda = TipoCambiario.Euro,
+					Monto = account.Monto,
+					Nombre = account.Nombre,
+				};
+			}
+			else if (account.Moneda.Equals(TipoCambiarioDTO.Dolar))
+			{
+				cuenta = new Ahorro()
+				{
+					FechaCreacion = account.FechaCreacion,
+					Moneda = TipoCambiario.Dolar,
+					Monto = account.Monto,
+					Nombre = account.Nombre,
+				};
+			}
+			else
+			{
+				cuenta = new Ahorro()
+				{
+					FechaCreacion = account.FechaCreacion,
+					Moneda = TipoCambiario.PesosUruguayos,
+					Monto = account.Monto,
+					Nombre = account.Nombre,
+				};
+			}
+			return cuenta;
+		}
+
+		private Credito CreditoDTO_A_Credito(CreditoDTO credit)
+		{
+			Credito cuenta;
+			if (credit.Moneda.Equals(TipoCambiarioDTO.Euro))
+			{
+				cuenta = new Credito()
+				{
+					NumeroTarjeta = credit.NumeroTarjeta,
+					BancoEmisor = credit.BancoEmisor,
+					CreditoDisponible = credit.CreditoDisponible,
+					FechaCierre = credit.FechaCierre,
+					Moneda = TipoCambiario.Euro,
+				};
+			}
+			else if (credit.Moneda.Equals(TipoCambiarioDTO.Dolar))
+			{
+				cuenta = new Credito()
+				{
+					NumeroTarjeta = credit.NumeroTarjeta,
+					BancoEmisor = credit.BancoEmisor,
+					CreditoDisponible = credit.CreditoDisponible,
+					FechaCierre = credit.FechaCierre,
+					Moneda = TipoCambiario.Dolar,
+				};
+			}
+			else
+			{
+				cuenta = new Credito()
+				{
+					NumeroTarjeta = credit.NumeroTarjeta,
+					BancoEmisor = credit.BancoEmisor,
+					CreditoDisponible = credit.CreditoDisponible,
+					FechaCierre = credit.FechaCierre,
+					Moneda = TipoCambiario.PesosUruguayos,
+				};
+			}
+			return cuenta;
+		}
+
+		private List<CategoriaDTO> CategoriasA_DTO(List<Categoria> categorias)
+		{
+			List<CategoriaDTO> categoriaDTOs = new List<CategoriaDTO>();
+			foreach (Categoria cat in categorias)
+			{
+				CategoriaDTO categoria = new CategoriaDTO()
+				{
+					Id = cat.Id,
+					EstadoActivo = cat.EstadoActivo,
+					FechaCreacion = cat.FechaCreacion,
+					Nombre = cat.Nombre,
+					Tipo = TipoCategoriaDTO.Costo,
+				};
+				categoriaDTOs.Add(categoria);
+			}
+			return categoriaDTOs;
+		}
+
+		private Categoria CategoriaDTO_A_Categoria(CategoriaDTO categoria)
+		{
+			Categoria categoriaRetornar;
+			if (categoria.Tipo.Equals(TipoCategoriaDTO.Costo))
+			{
+				categoriaRetornar = new Categoria()
+				{
+					EstadoActivo = categoria.EstadoActivo,
+					FechaCreacion = categoria.FechaCreacion,
+					Id = categoria.Id,
+					Nombre = categoria.Nombre,
+					Tipo = TipoCategoria.Costo,
+				};
+			}
+			else
+			{
+				categoriaRetornar = new Categoria()
+				{
+					EstadoActivo = categoria.EstadoActivo,
+					FechaCreacion = categoria.FechaCreacion,
+					Id = categoria.Id,
+					Nombre = categoria.Nombre,
+					Tipo = TipoCategoria.Ingreso,
+				};
+			}
+			return categoriaRetornar;
+		}
+
+	}
+}
