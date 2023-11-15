336d9035e0fe14349c57dff5a16c4f010f03803d
Mateo Arias <mateoarias437@gmail.com>
2023-11-15 01:20:28 -0300

    RED: ReporteListadoGastos_Crea_Elemento_Cuenta_Ahorro_Euros

--- FinTrac/ControladorTest/ControladorReporteTest.cs
+++ FinTrac/ControladorTest/ControladorReporteTest.cs
@@ -242,5 +242,78 @@ namespace ControladorTest
 			List<TransaccionDTO> reporte = controladorReporte.ReporteDeGastos(1, categoriaDTO, DateTime.Today.AddMonths(-2), DateTime.Today.AddMonths(2), ahorroDTO);
 			Assert.IsTrue(reporte.Count == 1);
 		}
+
+		[TestMethod]
+		public void ReporteListadoGastos_Crea_Elemento_Cuenta_Ahorro_Euros()
+		{
+			Usuario usuario = new Usuario
+			{
+				Nombre = "Usuario",
+				Apellido = "Test",
+				Correo = "test@gmail.com",
+				Contrasena = "TestTest12",
+				Direccion = "Av test"
+			};
+			_usuarioLogic.AddUsuario(usuario);
+			Espacio espacio = new Espacio
+			{
+				Nombre = "Espacio",
+				Id = 1,
+				Admin = usuario
+			};
+			Ahorro ahorro1 = new Ahorro
+			{
+				Nombre = "AhorroTest1",
+				Monto = 100,
+				FechaCreacion = DateTime.Now,
+				Moneda = TipoCambiario.Euro
+			};
+			espacio.AgregarCuenta(ahorro1);
+			Cambio cambioEuro = new Cambio()
+			{
+				FechaDeCambio = DateTime.Today,
+				Moneda = TipoCambiario.Euro,
+				Pesos = 30,
+			};
+			espacio.AgregarCambio(cambioEuro);
+			Categoria categoria = new Categoria()
+			{
+				Id = 1,
+				Nombre = "Categoria1",
+				Tipo = TipoCategoria.Costo,
+				EstadoActivo = true,
+				FechaCreacion = DateTime.Now
+			};
+			Transaccion transaccion1 = new Transaccion()
+			{
+				Id = 1,
+				Moneda = ahorro1.Moneda,
+				Monto = 1000,
+				CategoriaTransaccion = categoria,
+				CuentaMonetaria = ahorro1,
+				Titulo = "hola",
+				FechaTransaccion = DateTime.Today,
+			};
+			espacio.AgregarTransaccion(transaccion1);
+			_espacioLogic.AddEspacio(espacio);
+			CategoriaDTO categoriaDTO = new CategoriaDTO()
+			{
+				Id = categoria.Id,
+				EstadoActivo = true,
+				FechaCreacion = DateTime.Now,
+				Nombre = categoria.Nombre,
+				Tipo = TipoCategoriaDTO.Costo,
+			};
+			AhorroDTO ahorroDTO = new AhorroDTO()
+			{
+				FechaCreacion = ahorro1.FechaCreacion,
+				Moneda = TipoCambiarioDTO.Dolar,
+				Monto = ahorro1.Monto,
+				Nombre = ahorro1.Nombre,
+			};
+			ControladorReporte controladorReporte = new ControladorReporte(_espacioLogic);
+			List<TransaccionDTO> reporte = controladorReporte.ReporteDeGastos(1, categoriaDTO, DateTime.Today.AddMonths(-2), DateTime.Today.AddMonths(2), ahorroDTO);
+			Assert.IsTrue(reporte.Count != 1);
+		}
 	}
 }
