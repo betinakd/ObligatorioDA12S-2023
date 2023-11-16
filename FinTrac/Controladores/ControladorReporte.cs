f0605673401d7918f107c3ec2560573c06566067
Mateo Arias <mateoarias437@gmail.com>
2023-11-15 21:46:30 -0300

    REFACTOR: ReporteD con Controlador

--- FinTrac/Controladores/ControladorReporte.cs
+++ FinTrac/Controladores/ControladorReporte.cs
@@ -342,12 +342,5 @@ namespace Controlador
 			return categoriaRetornar;
 		}
 
-		public CambioDTO EncontrarCambio(int id, TransaccionDTO transaccion)
-		{
-			Espacio espacioActual = _reporte.FindEspacio(id);
-			ControladorEspacio controladorCuenta = new ControladorEspacio(_reporte);
-		controladorCuenta.
-		}
-
 	}
 }
